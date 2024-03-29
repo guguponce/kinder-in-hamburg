29ba8b4a3ace8370072ddc6b246c98239ca1d0a2
guguponce <augustoponceph@gmail.com>
2024-03-29 22:05:35 +0100

    RecommendedPosts component updated types

--- src/app/components/@Homepage/RecommendedPosts.tsx
+++ src/app/components/@Homepage/RecommendedPosts.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import CardsDisplay from "../CardsDisplay";
-import { iRetrievedPost } from "@app/utils/types";
+import { iParsedRetrievedPost, iPost } from "@app/utils/types";
 
 export default function RecommendedPosts({
   recommendedPosts,
 }: {
-  recommendedPosts: iRetrievedPost[];
+  recommendedPosts: iParsedRetrievedPost[];
 }) {
   return (
     <section className="w-full lg:w-4/5 max-w-[1000px] p-2 md:p-4 bg-hh-600 shadow-md relative flex flex-col items-center">
@@ -15,11 +15,7 @@ export default function RecommendedPosts({
       >
         Recommendations of the month
       </h2>
-      <CardsDisplay
-        id="recommendation"
-        cardType="homepage-recomendations"
-        cardPosts={recommendedPosts}
-      />
+      <CardsDisplay id="recommendation" cardPosts={recommendedPosts} />
     </section>
   );
 }
