fbcb8c782f88eb97a212f3ecb721c5b9f93b1fdf
guguponce <augustoponceph@gmail.com>
2024-04-05 23:52:39 +0200

    Post approvals

---
+++ src/app/posts-approval/PostsApproval.tsx
@@ -0,0 +1,49 @@
+"use client";
+import DisplayTypeText from "@app/components/@PostForm/DisplayTypeText";
+import MinimalPostDisplay from "@app/components/MinimalPostDisplay";
+import { iParsedRetrievedPost } from "@app/utils/types";
+import React, { useEffect, useState } from "react";
+
+export default function PostsApproval({
+  postsList,
+}: {
+  postsList: iParsedRetrievedPost[];
+}) {
+  const [selectedPost, setSelectedPost] = useState<number | null>(null);
+  const handleSelectPost = (postID: number) => setSelectedPost(postID);
+  return (
+    <main className="w-full">
+      {postsList.map((post) =>
+        post.id === selectedPost ? (
+          <article
+            key={post.id}
+            className="relative my-2 w-full rounded-sm bg-white mx-auto p-4 flex flex-col"
+          >
+            <MinimalPostDisplay post={post} />
+          </article>
+        ) : (
+          <article
+            key={post.id}
+            className="relative my-2 w-full rounded-sm bg-white mx-auto p-4 flex flex-col"
+          >
+            <button
+              className="absolute right-2 top-3 bg-hh-800 text-white rounded-md p-1"
+              onClick={() => handleSelectPost(post.id)}
+            >
+              Expand
+            </button>
+            <h2 className="font-semibold max-w-[calc(100%-60px)] mb-2">
+              {post.title}
+            </h2>
+            <DisplayTypeText
+              key={post.text[0][0]}
+              type={post.text[0][0]}
+              text={post.text[0][1]}
+            />
+            <small className="self-end">{post.addedBy.name}</small>
+          </article>
+        )
+      )}
+    </main>
+  );
+}
---
+++ src/app/posts-approval/[suggestionID]/page.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import PostForm from "@components/@PostForm/PostForm";
+import { getServerSession } from "next-auth";
+import {
+  getApprovedPostWithID,
+  getSuggestedPostWithID,
+} from "@app/api/dbActions";
+import { parseAddress, parsePost } from "@app/utils/functions";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+export default async function ApproveSuggestedPostPage({
+  params,
+}: {
+  params: { suggestionID: string };
+}) {
+  const session = await getServerSession();
+  if (!session?.user || session.user.email !== process.env.ADMIN_EMAIL)
+    redirect("/");
+  const { suggestionID } = params;
+
+  const approvedPost = await getApprovedPostWithID(suggestionID);
+
+  if (approvedPost)
+    return (
+      <main className="mx-auto flex h-fit w-full max-w-[400px] flex-col items-center justify-center rounded-md bg-hh-100 p-4 gap-4 text-center">
+        <h2 className="font-semibold text-xl">
+          The suggestion: {'"'}
+          {approvedPost.title}
+          {'"'} <br /> has already been approved
+        </h2>
+        <p>That means that it can not be changed anymore</p>
+        <Link
+          className="px-2 py-1 rounded-md font-semibold bg-hh-700 text-white hover:bg-hh-600"
+          href={`/posts/${suggestionID}`}
+        >
+          Go check it out!
+        </Link>
+        {session.user.email === process.env.ADMIN_EMAIL && (
+          <div className="flex flex-col items-center mt-4 p-4">
+            <h3 className="font-semibold text-xl">
+              You could though update the approved post
+            </h3>
+            <Link
+              className="px-2 py-1 rounded-md font-semibold bg-hh-700 text-white hover:bg-hh-600"
+              href={`/update-post/${suggestionID}`}
+            >
+              Update the approved post
+            </Link>
+          </div>
+        )}
+      </main>
+    );
+
+  const post = parsePost(await getSuggestedPostWithID(suggestionID));
+  return (
+    <section className="body-font relative mb-10 mt-6 max-w-[1000px] bg-[hsl(35,73%,57%,0.9)] p-4 text-gray-200 lg:mx-8">
+      <div className="h-full w-full bg-[rgb(255,255,255,0.6)] p-5 px-5">
+        <h1 className="title-font mb-4 text-center text-2xl font-bold text-gray-900 sm:text-3xl">
+          APPROVE POST
+        </h1>
+        <PostForm
+          postType="suggested-to-approved"
+          PostForm={{
+            ...post,
+            address: post.address ? parseAddress(post.address) : undefined,
+          }}
+          user={session.user}
+        />
+      </div>
+    </section>
+  );
+}
