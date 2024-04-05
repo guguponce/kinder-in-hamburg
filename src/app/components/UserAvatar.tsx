c3b7a11dcef91c2e750a745616361bcb3dba7052
guguponce <augustoponceph@gmail.com>
2024-04-05 23:56:00 +0200

    UserAvatar set

--- src/app/components/UserAvatar.tsx
+++ src/app/components/UserAvatar.tsx
@@ -1,24 +1,39 @@
-import { getServerSession } from "next-auth";
+"use client";
+import Link from "next/link";
 import React from "react";
 
-export default async function UserAvatar({
+export default function UserAvatar({
   avatar,
   name,
+  link,
 }: {
   name: string | undefined | null;
   avatar: string | undefined | null;
+  link: string;
 }) {
+  const [imgDisplay, setImgDisplay] = React.useState(false);
+  const imgRef = React.useRef<HTMLImageElement>(null);
   if (!avatar) return <></>;
+  const initials = name
+    ?.split(" ")
+    .slice(0, 2)
+    .map((n) => n[0])
+    .join("");
   return (
-    <div>
-      <img
-        className="w-8 h-8 rounded-full"
-        src={avatar}
-        alt={name
-          ?.split(" ")
-          .map((n) => n[0])
-          .join("")}
-      />
-    </div>
+    <Link
+      href={link}
+      className="relative w-8 h-8 flex justify-center items-center text-white font-bold text-sm rounded-full bg-hh-700 bg-contain bg-center"
+    >
+      {initials ? initials : ""}
+      {imgDisplay && (
+        <img
+          src={avatar}
+          className="absolute z-[500] w-full h-full rounded-full"
+          alt=""
+          ref={imgRef}
+          onError={() => setImgDisplay(false)}
+        />
+      )}
+    </Link>
   );
 }
