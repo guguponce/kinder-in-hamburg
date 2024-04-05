49ade0cd5f442c3a3281ecaa38fef9d15a231a25
guguponce <augustoponceph@gmail.com>
2024-04-05 23:51:27 +0200

    Icon components set

---
+++ src/app/components/@Icons/PostLogo.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function PostLogo({
+  size = "24px",
+  color = "#000",
+  logo,
+}: {
+  size?: string;
+  color?: string;
+  logo: string;
+}) {
+  return (
+    <>
+      {logo === "instagram" && (
+        <svg
+          fill={color}
+          width={size}
+          height={size}
+          viewBox="0 0 32 32"
+          id="Camada_1"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          ></g>
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <g>
+              {" "}
+              <path d="M22.3,8.4c-0.8,0-1.4,0.6-1.4,1.4c0,0.8,0.6,1.4,1.4,1.4c0.8,0,1.4-0.6,1.4-1.4C23.7,9,23.1,8.4,22.3,8.4z"></path>{" "}
+              <path d="M16,10.2c-3.3,0-5.9,2.7-5.9,5.9s2.7,5.9,5.9,5.9s5.9-2.7,5.9-5.9S19.3,10.2,16,10.2z M16,19.9c-2.1,0-3.8-1.7-3.8-3.8 c0-2.1,1.7-3.8,3.8-3.8c2.1,0,3.8,1.7,3.8,3.8C19.8,18.2,18.1,19.9,16,19.9z"></path>{" "}
+              <path d="M20.8,4h-9.5C7.2,4,4,7.2,4,11.2v9.5c0,4,3.2,7.2,7.2,7.2h9.5c4,0,7.2-3.2,7.2-7.2v-9.5C28,7.2,24.8,4,20.8,4z M25.7,20.8 c0,2.7-2.2,5-5,5h-9.5c-2.7,0-5-2.2-5-5v-9.5c0-2.7,2.2-5,5-5h9.5c2.7,0,5,2.2,5,5V20.8z"></path>{" "}
+            </g>{" "}
+          </g>
+        </svg>
+      )}
+      {logo === "link" && (
+        <svg
+          width={size}
+          height={size}
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+              d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212"
+              stroke={color}
+              strokeWidth="3"
+              strokeLinecap="round"
+            ></path>{" "}
+            <path
+              d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373"
+              stroke="#1C274C"
+              strokeWidth="3"
+              strokeLinecap="round"
+            ></path>{" "}
+          </g>
+        </svg>
+      )}
+    </>
+  );
+}
