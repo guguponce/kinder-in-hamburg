466e7d327803e14389366643177dd0da2ef9731f
guguponce <augustoponceph@gmail.com>
2024-04-06 00:00:02 +0200

    old CardDisplayComponents deleted

--- src/app/categories/[categories]/page.tsx
+++ src/app/categories/[categories]/page.tsx
@@ -1,7 +1,14 @@
-import { getPostWithCategory } from "@app/api/dbActions";
-import { parsePost } from "@app/utils/functions";
-import CardsDisplay from "@app/components/CardsDisplay";
+import { getSuggestionsWithCat } from "@app/api/dbActions";
+import {
+  getDescription,
+  getPlainText,
+  parseParams,
+  parsePost,
+} from "@app/utils/functions";
+import CardsDisplay from "@app/components/@Cards/CardsDisplay";
 import React from "react";
+import Card from "@app/components/@Cards/Card";
+import { categoryNames } from "@app/utils/constants";
 
 export default async function CurrentCategoryPage({
   params,
@@ -9,13 +16,36 @@ export default async function CurrentCategoryPage({
   params: { categories: string };
 }) {
   const { categories } = params;
-
-  const categoryPosts = await getPostWithCategory(categories);
-  if (!categoryPosts) return <div>Category not found</div>;
+  const category = parseParams(categories);
+  if (categoryNames.every((c) => c.toLowerCase() != category.toLowerCase()))
+    return <>{category} is not one of out Categories</>;
+  const categoryPosts = await getSuggestionsWithCat(category);
   const parsedPosts = categoryPosts.map((post) => parsePost(post));
   return (
-    <main>
-      <CardsDisplay id={"category"} cardPosts={parsedPosts} />
+    <main className="w-full  max-w-[1000px] p-4 flex flex-col items-center">
+      <h1 className="text-2xl font-bold">{category.toUpperCase()}</h1>
+      <section className="w-fit flex justify-center md:justify-around flex-wrap p-2 gap-4">
+        {!categoryPosts.length ? (
+          <div>No posts found for {category}</div>
+        ) : (
+          parsedPosts.map(({ id, title, text, image }) => (
+            <React.Fragment key={id}>
+              <Card
+                size="small"
+                id={id}
+                title={title}
+                image={
+                  image
+                    ? image[0]
+                    : `https://dummyimage.com/200x100/47596b/fff.jpg&text=${title}`
+                }
+                description={getPlainText(text)}
+              />
+            </React.Fragment>
+          ))
+        )}
+      </section>
+      {/* <CardsDisplay id={"category"} cardPosts={parsedPosts} /> */}
     </main>
   );
 }
