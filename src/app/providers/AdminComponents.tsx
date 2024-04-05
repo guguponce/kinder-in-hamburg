caa491f0ca38dbaccfcd76ead5137fe0035fa598
guguponce <augustoponceph@gmail.com>
2024-04-05 23:52:14 +0200

    Admin Components

---
+++ src/app/providers/AdminComponents.tsx
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default async function AdminRoute({
+  children,
+}: {
+  children: JSX.Element;
+}) {
+  const session = await getServerSession();
+  if (session?.user?.email !== "mockupap@gmail.com") return <></>;
+
+  return <>{children}</>;
+}
