fc1b145ee9e26e0401e6a363650d3b1e656a0d96
guguponce <augustoponceph@gmail.com>
2024-04-11 22:48:51 +0200

    FlohForm refactored
    -address/time
    -onsubmit/sleep

--- src/app/components/@FlohForm/UserInputBox.tsx
+++ src/app/components/@FlohForm/UserInputBox.tsx
@@ -11,7 +11,7 @@ export default function UserInputBox({
 }) {
   const keys: ("name" | "email")[] = ["name", "email"];
   return (
-    <section className="userInputBox w-full max-w-[800px] mx-auto justify-center flex flex-wrap items-center gap-2">
+    <section className="userInputBox w-full max-w-[800px] mx-auto justify-center flex flex-wrap items-center gap-2 text-hh-950 mb-2">
       {keys.map((k) => (
         <div className="w-[calc(50%-1rem)] max-w-[300px]" key={k}>
           <PostFormInput
@@ -19,11 +19,11 @@ export default function UserInputBox({
             inputID={k}
             required={true}
           >
-            {userInput[k] ? (
+            {!userInput[k] ? (
               <input
                 type={k === "email" ? "email" : "text"}
                 id={k}
-                className="w-full rounded border border-gray-300 bg-gray-100 bg-opacity-60 px-3 py-1 text-base font-semibold leading-8 text-gray-900 outline-none transition-colors duration-200 ease-in-out focus:border-hh-600 focus:bg-white focus:ring-2 focus:ring-hh-700"
+                className="w-full rounded border border-hh-300 bg-hh-100 bg-opacity-60 px-3 py-1 text-base font-semibold leading-8 text-hh-900 outline-none transition-colors duration-200 ease-in-out focus:border-hh-600 focus:bg-white focus:ring-2 focus:ring-hh-700"
                 name={k}
                 value={userInput[k] || ""}
                 onChange={(e) =>
@@ -31,7 +31,7 @@ export default function UserInputBox({
                 }
               />
             ) : (
-              <h3 className="w-full rounded border border-gray-300  px-3 py-1 text-base font-semibold leading-8 text-gray-900 outline-none transition-colors duration-200 ease-in-out focus:border-hh-600 focus:bg-white focus:ring-2 focus:ring-hh-700">
+              <h3 className="w-full rounded border-hh-400 border-2  px-3 py-1 text-base font-semibold leading-8 text-hh-900 outline-none transition-colors duration-200 ease-in-out focus:border-hh-600 focus:bg-white focus:ring-2 focus:ring-hh-700">
                 {userInput[k]}
               </h3>
             )}
