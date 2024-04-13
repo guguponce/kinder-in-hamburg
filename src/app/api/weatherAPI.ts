31f1cae9fea7eb42aa5627fc04d8a9f33a5cf9bf
guguponce <augustoponceph@gmail.com>
2024-04-13 10:55:41 +0200

    revalidateTag in weather fetching

--- src/app/api/weatherAPI.ts
+++ src/app/api/weatherAPI.ts
@@ -1,11 +1,13 @@
 "use server";
 
 import { WeatherAPI } from "@app/utils/types";
+import { revalidateTag } from "next/cache";
 
 export const getHamburgsWeather = async () => {
+  revalidateTag("weather");
   const response = await fetch(
     `https://api.weatherapi.com/v1/forecast.json?key=${process.env.WEATHER_API}&q=53.5511,9.9937&days=3&aqi=no&alerts=no`,
-    { next: { revalidate: 40 } }
+    { next: { tags: ["weather"] } }
   );
   const data = await response.json();
   return data as WeatherAPI;
