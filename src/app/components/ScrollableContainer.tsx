161c07a77b3ceb5f6fa3bd085f8e48c635743c1b
guguponce <augustoponceph@gmail.com>
2024-04-15 12:39:13 +0200

    vertical option in ScrollableContainer

--- src/app/components/ScrollableContainer.tsx
+++ src/app/components/ScrollableContainer.tsx
@@ -2,12 +2,24 @@ import React from "react";
 
 export default function ScrollableContainer({
   children,
+  vertical = false,
 }: {
+  vertical?: boolean;
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex  items-center w-fit max-w-full overflow-hidden">
-      <div className="horizontalScrollbar overflow-x-auto w-fit max-w-full flex gap-2 items-stretch px-4 pb-4 pt-2">
+    <div
+      className={`flex items-center ${
+        vertical ? "w-full" : "w-fit max-w-full"
+      } overflow-hidden`}
+    >
+      <div
+        className={`${
+          vertical
+            ? "verticalScrollbar w-full overflow-y-auto h-fit max-h-full flex-col"
+            : "horizontalScrollbar overflow-x-auto w-fit max-w-full px-4 pb-4 pt-2"
+        }  flex gap-2 items-stretch `}
+      >
         {children}
       </div>
     </div>
