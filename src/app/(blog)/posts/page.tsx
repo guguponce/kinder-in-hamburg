d600a84c5fffc531c8beffe33ca45b1b5e451eae
guguponce <augustoponceph@gmail.com>
2024-04-15 12:44:30 +0200

    Posts page with FilterablePostsList

--- src/app/(blog)/posts/page.tsx
+++ src/app/(blog)/posts/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { getAllApprovedPosts } from "@app/api/dbActions";
 import CardsDisplay from "@components/@Cards/CardsDisplay";
+import FilterablePostList from "@app/components/FilterablePostList";
 
 export default async function PostsPage() {
   const postsList = await getAllApprovedPosts();
   if (!postsList) return <div>There was a problem retrieving posts</div>;
-  return <CardsDisplay cardPosts={postsList} />;
+  return (
+    <FilterablePostList postsList={postsList}>
+      <h1 className="font-bold min-w-fit text-center   text-3xl ">All posts</h1>
+    </FilterablePostList>
+  );
 }
