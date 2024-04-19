a8b03edb8678a1a7ee55c1851a8223576725dc95
guguponce <augustoponceph@gmail.com>
2024-04-19 18:14:55 +0200

    FullImageWeatherBox refactored

--- src/app/components/FullImageWeatherBox.tsx
+++ src/app/components/FullImageWeatherBox.tsx
@@ -8,6 +8,7 @@ export default function FullImageWeatherBox({
   imgURL,
   prefixLink,
   id,
+  weatherAtRight,
 }: {
   id: number;
   children: React.ReactNode;
@@ -15,10 +16,13 @@ export default function FullImageWeatherBox({
   text?: string;
   imgURL: string;
   prefixLink?: string;
+  weatherAtRight?: boolean;
 }) {
   return (
     <aside
-      className={`bg-sky bg-opacity-20 text-hh-700 flex flex-col gap-4 md:flex-row items-center justify-center rounded w-full max-w-[1000px] bg-cover bg-center bg-no-repeat min-h-[60vh] p-4`}
+      className={`bg-sky bg-opacity-20 text-hh-700 flex flex-col gap-4 ${
+        weatherAtRight ? "md:flex-row-reverse" : "md:flex-row"
+      } items-stretch justify-center rounded w-full h-full bg-cover bg-center bg-no-repeat p-4`}
       style={{ backgroundImage: `url(${imgURL})` }}
     >
       {children}
@@ -27,13 +31,13 @@ export default function FullImageWeatherBox({
           {title}
         </h3>
         {text && (
-          <h3 className={`text-hh-200 italic max-w-full break-words`}>
+          <h3 className="truncate-4 text-hh-200 italic max-w-full break-words text-xs">
             {text}
           </h3>
         )}
         <Link
           href={`${prefixLink || "/posts/"}${id}`}
-          className="self-end px-2 py-1 font-semibold capitalize rounded-sm text-white bg-hh-400 hover:bg-hh-500 active:bg-hh-300 break-words w-fit text-center"
+          className="self-end px-2 py-1 mt-1 font-semibold capitalize rounded-sm text-white bg-hh-400 hover:bg-hh-500 active:bg-hh-300 break-words w-fit text-center"
         >
           Read more
         </Link>
