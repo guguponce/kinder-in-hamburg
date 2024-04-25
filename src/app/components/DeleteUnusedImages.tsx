e16db6c8981df41ee5010a8861cef4d3f3704aa8
guguponce <augustoponceph@gmail.com>
2024-04-26 01:05:48 +0200

    deleteUnusedImages fixed

--- src/app/components/DeleteUnusedImages.tsx
+++ src/app/components/DeleteUnusedImages.tsx
@@ -1,10 +1,8 @@
 "use client";
-import { getAllFlohs, getSuggestedFlohmaerkte } from "@app/api/dbActions";
 import {
   deleteUnusedFlohmaerkteImages,
   deleteUnusedImages,
 } from "@app/api/storageActions";
-import { iFlohmarkt } from "@app/utils/types";
 import React from "react";
 
 export default function DeleteUnusedImages() {
