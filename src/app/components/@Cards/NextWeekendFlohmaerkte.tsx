2ecfd328e99a59ef14bcdbfd5c76a9118f868b9a
guguponce <augustoponceph@gmail.com>
2024-04-25 14:09:00 +0200

    FlohPostersList refactored
    -dashboard ux fixed

--- src/app/components/@Cards/NextWeekendFlohmaerkte.tsx
+++ src/app/components/@Cards/NextWeekendFlohmaerkte.tsx
@@ -2,6 +2,7 @@ import { getThisWeekFlohmaerkte } from "@app/api/dbActions";
 import React from "react";
 import ScrollableContainer from "../ScrollableContainer";
 import FlohmarktPoster from "../@PostForm/FlohmarktPoster";
+import FlohPostersList from "../FlohPostersList";
 
 export default async function ThisWeekFlohmaerkte() {
   const thisWeek = await getThisWeekFlohmaerkte();
@@ -10,19 +11,7 @@ export default async function ThisWeekFlohmaerkte() {
   return (
     <section className="nextWeekendFlohmarkt bg-hh-100 bg-opacity-10 p-2 w-full flex justify-center gap-2 items-center">
       <ScrollableContainer>
-        {sortedFlohmaerkte.map(({ title, image, date, id, bezirk }) => (
-          <React.Fragment key={id}>
-            <article className="w-full sm:w-1/2 min-w-[180px] max-w-[200px] min-h-[270px] h-auto bg-hh-200 rounded ">
-              <FlohmarktPoster
-                id={id}
-                title={title}
-                image={image}
-                date={date}
-                bezirk={bezirk}
-              />
-            </article>{" "}
-          </React.Fragment>
-        ))}
+        <FlohPostersList flohList={sortedFlohmaerkte} />
       </ScrollableContainer>
     </section>
   );
--- src/app/components/FlohPostersList.tsx
+++ src/app/components/FlohPostersList.tsx
@@ -14,14 +14,13 @@ export default function FlohPostersList({
       {!!flohList.length &&
         flohList.map(({ title, image, date, id, bezirk }) => (
           <React.Fragment key={id}>
-            <article className="w-full sm:w-1/2 min-w-[150px] max-w-[180px] md:min-w-[180px] md:max-w-[200px] min-h-[270px]   h-auto bg-hh-200 rounded ">
+            <article className="w-full sm:w-1/2 min-w-[180px] max-w-[200px] min-h-[270px] h-auto bg-hh-200 rounded ">
               <FlohmarktPoster
                 id={id}
                 title={title}
                 image={image}
                 date={date}
                 bezirk={bezirk}
-                prefixLink={prefixLink || ""}
               />
             </article>{" "}
           </React.Fragment>
