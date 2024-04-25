139b9b947e855af22bc0bf043b526d39df591688
guguponce <augustoponceph@gmail.com>
2024-04-25 13:58:49 +0200

    flohmaerkte futureFloh sorted

--- src/app/(flohmaerkte)/flohmaerkte/page.tsx
+++ src/app/(flohmaerkte)/flohmaerkte/page.tsx
@@ -27,7 +27,9 @@ export default async function FlohmarktPage() {
   const nextWeekendFlohmaerkte = flohmaerkte.filter(
     ({ date }) => date > nextSaturday && date < nextMonday
   );
-  const futureFlohmaerkte = flohmaerkte.filter(({ date }) => date > nextMonday);
+  const futureFlohmaerkte = flohmaerkte
+    .filter(({ date }) => date > nextMonday)
+    .sort((a, b) => a.date - b.date);
   return (
     <main className="rounded bg-hh-100 bg-opacity-25 w-full max-w-[1000px] p-4 flex flex-col items-center min-h-[50vh] gap-2">
       <h1 className="text-4xl font-bold my-2 p-2 rounded bg-opacity-50 bg-hh-50">
