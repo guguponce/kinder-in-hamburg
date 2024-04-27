5cb9ccf3d47492e84fcfa9d461ab3c0447ac24c4
guguponce <augustoponceph@gmail.com>
2024-04-28 00:11:20 +0200

    Protected Header

--- src/app/components/@Header/Header.tsx
+++ src/app/components/@Header/Header.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import BurgerMenu from "./BurgerMenu";
 import UserButtons from "./UserButtons";
 import AdminServerComponent from "@app/providers/AdminServerComponents";
+import AdminClientComponent from "@app/providers/AdminClientComponents";
 
 export default async function Header() {
   return (
@@ -12,7 +13,9 @@ export default async function Header() {
       id="header"
       className="relative h-20 w-full lg:max-w-[1200px] bg-negative-500 p-2 sm:p-4 flex justify-between lg:justify-center items-center gap-2 text-white"
     >
-      <BurgerMenu />
+      <AdminClientComponent>
+        <BurgerMenu />
+      </AdminClientComponent>
       <div
         className="mx-auto h-full px-4 flex-grow  rounded-sm bg-white flex items-center  justify-end lg:justify-center   hover:text-[#121212] active:text-[#121212] "
         id="logo-text"
@@ -24,30 +27,30 @@ export default async function Header() {
           Kinder in HH
         </Link>
       </div>
-      <nav className="hidden font-semibold lg:flex items-center gap-4">
-        {[
-          { href: "/posts", name: "Posts", auth: true },
-          { href: "/flohmaerkte", name: "Flohmärkte", auth: true },
-          { href: "/categories", name: "Categories", auth: true },
-          { href: "/bezirke", name: "Bezirke", auth: true },
-        ].map(({ href, name, auth }) =>
-          auth ? (
-            <React.Fragment key={href}>
-              <AdminServerComponent>
+      <AdminServerComponent>
+        <nav className="hidden font-semibold lg:flex items-center gap-4">
+          {[
+            { href: "/posts", name: "Posts", auth: true },
+            { href: "/flohmaerkte", name: "Flohmärkte", auth: true },
+            { href: "/categories", name: "Categories", auth: true },
+            { href: "/bezirke", name: "Bezirke", auth: true },
+          ].map(({ href, name, auth }) =>
+            auth ? (
+              <React.Fragment key={href}>
                 <Link key={href} href={href}>
                   {name}
                   <LinkActive linkHref={href} />
                 </Link>
-              </AdminServerComponent>
-            </React.Fragment>
-          ) : (
-            <Link key={href} href={href}>
-              {name}
-              <LinkActive linkHref={href} />
-            </Link>
-          )
-        )}
-      </nav>
+              </React.Fragment>
+            ) : (
+              <Link key={href} href={href}>
+                {name}
+                <LinkActive linkHref={href} />
+              </Link>
+            )
+          )}
+        </nav>
+      </AdminServerComponent>
       <AdminServerComponent>
         <div className="lg:ml-4 flex gap-2 items-center">
           <UserButtons />
