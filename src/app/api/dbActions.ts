d68f880309c36ff84869a2331ef8e76f1dd40715
guguponce <augustoponceph@gmail.com>
2024-04-27 23:57:55 +0200

    dbAction args

--- src/app/api/dbActions.ts
+++ src/app/api/dbActions.ts
@@ -20,8 +20,8 @@ import {
 } from "@app/utils/functions";
 import { deletePreviousFlohmaerkteImages } from "./storageActions";
 const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? ""
+  process.env.SUPABASE_URL ?? "",
+  process.env.SUPABASE_ANON_KEY ?? ""
 );
 
 //images
