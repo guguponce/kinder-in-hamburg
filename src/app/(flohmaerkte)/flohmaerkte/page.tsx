13d0bcf1fd1d20c99865e2b75b40d293f257cfa4
guguponce <augustoponceph@gmail.com>
2024-04-27 09:52:32 +0200

    nextWeekendFlohmaerkte is now thisWeekFlohmaerkte

--- src/app/(flohmaerkte)/flohmaerkte/page.tsx
+++ src/app/(flohmaerkte)/flohmaerkte/page.tsx
@@ -23,20 +23,21 @@ export default async function FlohmarktPage() {
         </Link>
       </main>
     );
-  const { nextSaturday, nextMonday } = getNextWeekend();
+  const { today, nextMonday } = getNextWeekend();
   const nextWeekendFlohmaerkte = flohmaerkte.filter(
-    ({ date }) => date > nextSaturday && date < nextMonday
+    ({ date }) => date > today && date < nextMonday
   );
   const futureFlohmaerkte = flohmaerkte
     .filter(({ date }) => date > nextMonday)
     .sort((a, b) => a.date - b.date);
+
   return (
     <main className="rounded bg-hh-100 bg-opacity-25 w-full max-w-[1000px] p-4 flex flex-col items-center min-h-[50vh] gap-2">
       <h1 className="text-4xl font-bold my-2 p-2 rounded bg-opacity-50 bg-hh-50">
         Flea Markets
       </h1>
       <ScrollableFlohmaerkte
-        title="Next Weekend's"
+        title="This Week's"
         flohmaerkte={nextWeekendFlohmaerkte}
       ></ScrollableFlohmaerkte>
       <BezirkableFlohmaerkteList
