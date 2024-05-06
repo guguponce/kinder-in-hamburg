e6be41d6e3325d449c41014e367ae6e34378edb2
guguponce <augustoponceph@gmail.com>
2024-05-06 12:01:36 +0200

    PaperPlane own component

--- src/app/(other-pages)/contact/page.tsx
+++ src/app/(other-pages)/contact/page.tsx
@@ -1,21 +1,6 @@
+import PaperPlane from "@app/components/@Icons/PaperPlane";
 import React from "react";
 
-function PaperPlane() {
-  return (
-    <svg
-      fill="#fefefe"
-      width="1rem"
-      height="1rem"
-      viewBox="0 0 32 32"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g id="SVGRepo_iconCarrier">
-        <path d="M0 14.016l9.216 6.912 18.784-16.928-14.592 20.064 10.592 7.936 8-32zM8 32l6.016-4-6.016-4v8z"></path>{" "}
-      </g>
-    </svg>
-  );
-}
 export default function ContactPage() {
   return (
     <main className="rounded-lg p-4 w-full max-w-[600px] bg-hh-100 text-hh-900 bg-opacity-90 flex flex-col gap-2">
---
+++ src/app/components/@Icons/PaperPlane.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function PaperPlane() {
+  return (
+    <svg
+      fill="#fefefe"
+      width="1rem"
+      height="1rem"
+      viewBox="0 0 32 32"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_iconCarrier">
+        <path d="M0 14.016l9.216 6.912 18.784-16.928-14.592 20.064 10.592 7.936 8-32zM8 32l6.016-4-6.016-4v8z"></path>{" "}
+      </g>
+    </svg>
+  );
+}
