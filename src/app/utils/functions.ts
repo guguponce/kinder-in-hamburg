063fd18b87e63bc82433d5cd960c8ad475ea71b7
guguponce <augustoponceph@gmail.com>
2024-06-07 00:05:04 +0200

    functions added isType - haversineDistance - createStandortMapIcon - parseHaynsParkXML

--- src/app/utils/functions.ts
+++ src/app/utils/functions.ts
@@ -314,3 +314,100 @@ export const parseSpielplatz = (spielplatz: iStringifiedSpielplatz) => {
       : [],
   } as iSpielplatz;
 };
+
+export const getHaynsParkXML = async () => {
+  const response = await fetch(
+    "https://planschis-eimsbuettel.blogspot.com/feeds/posts/default"
+  );
+  const xmlString = await response.text();
+  return xmlString;
+};
+
+export const parseHaynsParkXML = (xmlString: string) => {
+  const parser = new DOMParser();
+  const xmlDoc = parser.parseFromString(xmlString, "text/xml");
+  const titleNode = xmlDoc.getElementsByTagName("title"); // Get the first <title> element
+  let titles = [];
+  for (let i = 0; i < titleNode.length; i++) {
+    if (!!titleNode[i].textContent) {
+      titles.push(titleNode[i].textContent!);
+    }
+  }
+  return titles;
+};
+
+const datePattern = /(\d{1,2})\.(\d{1,2})\./g;
+
+export const extractDates = (str: string) => {
+  let match = datePattern.exec(str);
+  if (!match) return null;
+
+  return {
+    day: parseInt(match[1], 10),
+    month: parseInt(match[2], 10),
+    planschi: [
+      "schlump",
+      "weberspark",
+      "wehberspark",
+      "wehbers park",
+      "weiher",
+    ].filter((p) => str.includes(p)),
+  };
+};
+
+export const isToday = (date: {
+  day: number;
+  month: number;
+  planschi: string[];
+}) => {
+  const today = new Date();
+  return date.day === today.getDate() && date.month === today.getMonth() + 1;
+};
+
+export const getFirstDate = (strings: string[]) => {
+  for (let i = 0; i < strings.length; i++) {
+    const date = extractDates(strings[i]);
+    if (date) return date;
+  }
+  return null;
+};
+
+export function isTypeSpielplatz(item: any) {
+  return "spielgeraete" in item;
+}
+export function isTypeFlohmarkt(item: any) {
+  return "date" in item;
+}
+export function isTypePost(item: any) {
+  return "categories" in item;
+}
+
+export function haversineDistance(
+  lat1: number,
+  lon1: number,
+  lat2: number,
+  lon2: number
+) {
+  const toRadians = (degree: number) => degree * (Math.PI / 180);
+
+  const R = 6371000; // Radius of the Earth in meters
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(toRadians(lat1)) *
+      Math.cos(toRadians(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c;
+}
+
+export function createStandortMapIcon(
+  color: string = "#39579D",
+  size: number = 30
+) {
+  return `<svg fill=${color} height="${size}" width="${size}" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 511.953 511.953" stroke="#F6AA1C"><g id="SVGRepo_iconCarrier"> <g transform="translate(-1)"> <g> <g> <path d="M256.995,149.287c-11.776,0-21.333,9.579-21.333,21.333c0,11.755,9.557,21.333,21.333,21.333s21.333-9.579,21.333-21.333 C278.328,158.865,268.771,149.287,256.995,149.287z"/> <path d="M365.518,38.887C325.987,6.311,274.04-6.639,223.011,3.239C154.147,16.615,100.152,72.273,88.718,141.735 c-6.784,41.003,0.725,81.216,21.696,116.267l8.704,14.528c27.861,46.443,56.64,94.485,79.701,143.893l38.848,83.221 c3.499,7.509,11.029,12.309,19.328,12.309s15.829-4.8,19.328-12.309l34.965-74.923c23.317-49.984,52.096-98.688,79.957-145.792 l12.971-22.016c15.339-26.091,23.445-55.936,23.445-86.293C427.662,119.484,405.006,71.463,365.518,38.887z M256.995,234.62 c-35.285,0-64-28.715-64-64s28.715-64,64-64s64,28.715,64,64S292.28,234.62,256.995,234.62z"/> </g> </g> </g> </g></svg>`;
+}
