33e33542c9800356fab9543245e929e28d26a08e
guguponce <augustoponceph@gmail.com>
2024-06-07 23:21:25 +0200

    New-Spielplatz Page created

---
+++ src/app/(spielplatz)/new-spielplatz/page.tsx
@@ -0,0 +1,30 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import React from "react";
+import dynamic from "next/dynamic";
+import AdminServerComponent from "@app/providers/AdminServerComponents";
+
+const DynamicForm = dynamic(() => import("../SpielplatzForm"), {
+  ssr: false,
+});
+
+export default async function NewSpielplatzPage() {
+  const session = await getServerSession();
+  if (!session?.user) redirect("/api/auth/signin");
+  return (
+    <AdminServerComponent>
+      <main className="relative mb-10 mt-6 max-w-[1000px] w-full bg-hh-100 rounded-xl p-4 text-gray-200 lg:mx-8">
+        <div className="h-full w-full bg-hh-200 p-5 px-5">
+          <h1 className="title-font mb-4 text-center text-2xl font-medium text-gray-900 sm:text-3xl">
+            Add a new Flohmarkt
+          </h1>
+          <DynamicForm
+            spielplatzFormType="new-spielplatz"
+            spielplatzForm={{}}
+            user={{ ...session.user }}
+          />
+        </div>
+      </main>
+    </AdminServerComponent>
+  );
+}
