89846c467bb648b24dda753db9a6a5d899933c48
guguponce <augustoponceph@gmail.com>
2024-06-07 23:26:27 +0200

    AdminRoute added to new-spielplatz Page

--- src/app/(spielplatz)/new-spielplatz/page.tsx
+++ src/app/(spielplatz)/new-spielplatz/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import React from "react";
 import dynamic from "next/dynamic";
-import AdminServerComponent from "@app/providers/AdminServerComponents";
+import AdminRoute from "@app/providers/AdminRoute";
 
 const DynamicForm = dynamic(() => import("../SpielplatzForm"), {
   ssr: false,
@@ -12,7 +12,7 @@ export default async function NewSpielplatzPage() {
   const session = await getServerSession();
   if (!session?.user) redirect("/api/auth/signin");
   return (
-    <AdminServerComponent>
+    <AdminRoute>
       <main className="relative mb-10 mt-6 max-w-[1000px] w-full bg-hh-100 rounded-xl p-4 text-gray-200 lg:mx-8">
         <div className="h-full w-full bg-hh-200 p-5 px-5">
           <h1 className="title-font mb-4 text-center text-2xl font-medium text-gray-900 sm:text-3xl">
@@ -25,6 +25,6 @@ export default async function NewSpielplatzPage() {
           />
         </div>
       </main>
-    </AdminServerComponent>
+    </AdminRoute>
   );
 }
