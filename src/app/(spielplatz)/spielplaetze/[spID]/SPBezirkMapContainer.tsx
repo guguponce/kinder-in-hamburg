8f866dcb10249583765b20f9ef01cd824d4d1971
guguponce <augustoponceph@gmail.com>
2024-06-16 00:47:33 +0200

    SPBezirkMapContainer  loading changed

--- src/app/(spielplatz)/spielplaetze/[spID]/SPBezirkMapContainer.tsx
+++ src/app/(spielplatz)/spielplaetze/[spID]/SPBezirkMapContainer.tsx
@@ -15,11 +15,17 @@ export const revalidate = 10;
 const DynamicBezirkSPMap = dynamic(() => import("./SPBezirkMap"), {
   ssr: false,
   loading: () => (
-    <img
-      src="/assets/bezirke/mitte.webp"
-      className="object-cover w-full h-full"
-      alt=""
-    />
+    <div
+      className={`w-full max-w-[400px] md:max-w-full h-[400px] aspect-[0.5] shadow-md bg-hh-700 bg-opacity-90  flex flex-col md:flex-row lg:flex-col items-stretch lg:items-center p-2 gap-2 rounded mx-auto`}
+    >
+      <article className="w-full md:w-1/2 lg:w-full max-h-fit h-1/2 md:h-full lg:h-1/2 flex-grow  flex flex-col items-center gap-2 rounded bg-hh-900">
+        <img
+          src="/assets/bezirke/hamburg.webp"
+          className="object-cover w-full h-full"
+          alt=""
+        />
+      </article>
+    </div>
   ),
 });
 
