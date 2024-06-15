3668f4a9317cb70c9bf1708a07a43a099461ebea
guguponce <augustoponceph@gmail.com>
2024-06-16 00:33:38 +0200

    GeneralMap created

---
+++ src/app/components/@Map/GeneralMap.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import { divIcon } from "leaflet";
+import {
+  iAddress,
+  iFlohmarkt,
+  iFlohmarktWithCoordinates,
+  iPost,
+  iPostWithCoordinates,
+  iSpielplatz,
+} from "@app/utils/types";
+import Link from "next/link";
+import {
+  createStandortMapIcon,
+  getDate,
+  isTypeFlohmarkt,
+  isTypePost,
+  isTypeSpielplatz,
+  joinAddress,
+} from "@app/utils/functions";
+import SpielplatzPopUP from "@app/components/@Map/PopUpsMarkers/SpielplatzPopUP";
+import FlohmarktPopUP from "./PopUpsMarkers/FlohmarktPopUP";
+import PostPopUP from "./PopUpsMarkers/PostPopUP";
+
+const MainLocationIcon = divIcon({
+  html: createStandortMapIcon("#BC251F", 35),
+  className: "bg-transparent",
+  iconSize: [35, 35],
+  iconAnchor: [17, 35],
+});
+
+const GeneralMap = ({
+  currentTarget,
+  children,
+  zoom = 10,
+}: {
+  zoom?: number;
+  children?: React.ReactNode;
+  currentTarget?: iPost | iFlohmarkt | iSpielplatz;
+}) => {
+  return (
+    <MapContainer
+      style={{ height: "100%", width: "100%", zIndex: 10 }}
+      center={[currentTarget?.lat || 53.5511, currentTarget?.lon || 9.9937]}
+      zoom={zoom}
+      scrollWheelZoom={true}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {currentTarget?.lat && currentTarget?.lon && (
+        <Marker
+          position={[currentTarget.lat, currentTarget.lon]}
+          icon={MainLocationIcon}
+        >
+          {isTypeSpielplatz(currentTarget) ? (
+            <SpielplatzPopUP
+              title={(currentTarget as iSpielplatz).title}
+              id={currentTarget.id}
+              type={(currentTarget as iSpielplatz).type}
+              spielgeraete={(currentTarget as iSpielplatz).spielgeraete || []}
+              address={
+                currentTarget.address
+                  ? joinAddress(currentTarget.address as iAddress)
+                  : ""
+              }
+            />
+          ) : isTypeFlohmarkt(currentTarget) ? (
+            <FlohmarktPopUP
+              address={currentTarget.address as string}
+              date={(currentTarget as iFlohmarkt).date}
+              id={currentTarget.id}
+              title={(currentTarget as iFlohmarkt).title}
+            />
+          ) : (
+            isTypePost(currentTarget) && (
+              <PostPopUP
+                address={currentTarget.address as iAddress}
+                categories={(currentTarget as iPostWithCoordinates).categories}
+                id={currentTarget.id}
+                title={(currentTarget as iPostWithCoordinates).title}
+              />
+            )
+          )}
+        </Marker>
+      )}
+      {children}
+    </MapContainer>
+  );
+};
+
+export default GeneralMap;
