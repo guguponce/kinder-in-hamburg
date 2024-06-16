f203f5227c4acb51af362a646d40a7edafc36959
guguponce <augustoponceph@gmail.com>
2024-06-16 17:02:42 +0200

    ArrowGallery refactored style for small devices

--- src/app/components/ArrowGallery.tsx
+++ src/app/components/ArrowGallery.tsx
@@ -15,32 +15,32 @@ export default function ArrowGallery({
     <div
       id="arrow-gallery"
       className={`${
-        length > 1 ? "aspect-[0.9]" : "w-fit"
-      }h-full max-h-full flex flex-col items-center gap-4 bg-gray-500 bg-opacity-25 rounded`}
+        length > 1 ? "aspect-[0.75] lg:aspect-[0.9]" : "w-fit"
+      } h-full w-full xs:w-fit max-h-full flex justify-center gap-4 bg-gray-500 bg-opacity-25 rounded`}
     >
-      <div className="flex gap-1 items-center flex-grow justify-between w-full rounded py-2">
+      <div className="relative  flex gap-1 items-center flex-grow justify-center lg:justify-between w-full rounded">
         <div
           className={`${
             length > 1 ? "flex" : "hidden"
-          } h-full min-w-fit p-2 justify-center items-center rounded`}
+          } absolute lg:relative left-0 h-full min-w-fit z-10 p-2 justify-center items-center rounded`}
         >
           <button
-            className="min-h-12 min-w-12 p-2  rounded-full z-50 border  border-hh-100 hover:bg-hh-100 transition-all text-white flex justify-center items-center -rotate-90"
+            className="min-h-12 min-w-12 p-2 backdrop-blur-[1px] bg-hh-950 bg-opacity-10 rounded-full z-50 border  border-hh-100 hover:bg-hh-100 transition-all text-white flex justify-center items-center -rotate-90"
             onClick={() => handleIndex("back")}
           >
             <TriangleIcon color="#fefefe" size="1rem" />
           </button>
         </div>
-        <div className="w-full aspect-[0.66] max-w-[180px] overflow-hidden flex-grow flex justify-center items-center rounded  border-gray-200">
+        <div className="max-h-72  h-auto lg:max-w-[180px] overflow-hidden flex-grow flex justify-center items-center rounded  border-gray-200">
           {children}
         </div>
         <div
           className={`${
             length > 1 ? "flex" : "hidden"
-          } h-full min-w-fit p-2 justify-center items-center rounded`}
+          } absolute lg:relative right-0 h-full min-w-fit z-10 p-2 justify-center items-center rounded`}
         >
           <button
-            className="min-h-12 min-w-12 p-2  rounded-full z-50 border border-hh-100 hover:bg-hh-100 transition-all text-white flex justify-center items-center rotate-90"
+            className="min-h-12 min-w-12 p-2  backdrop-blur-[1px] bg-hh-950 bg-opacity-10  rounded-full z-50 border border-hh-100 hover:bg-hh-100 transition-all text-white flex justify-center items-center rotate-90"
             onClick={() => handleIndex("next")}
           >
             <TriangleIcon color="#fefefe" size="1rem" />
