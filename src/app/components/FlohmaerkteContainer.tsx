c25bf07459b10d68da4037d1f89e651e3dabdc80
guguponce <augustoponceph@gmail.com>
2024-06-16 13:36:35 +0200

    TodaysFlohmaerkte created

--- src/app/components/FlohmaerkteContainer.tsx
+++ src/app/components/FlohmaerkteContainer.tsx
@@ -1,4 +1,4 @@
-import ScrollableFlohmaerkte from "@app/components/ScrollableFlohmaerkte";
+import BezirkeScrollableFlohmaerkte from "@app/components/BezirkeScrollableFlohmaerkte";
 import React from "react";
 import BezirkableFlohmaerkteList from "../(flohmaerkte)/flohmaerkte/BezirkableFlohmaerkteList";
 import { getTodayNexMonday } from "@app/utils/functions";
@@ -9,6 +9,7 @@ const FlohmaerkteMap = dynamic(() => import("./@Map/FlohmaerkteMap"), {
   ssr: false,
 });
 import dynamic from "next/dynamic";
+import TodaysFlohmaerkte from "./TodaysFlohmaerkte";
 
 export const revalidate = 0;
 export default async function FlohmaerkteContainer() {
@@ -29,6 +30,7 @@ export default async function FlohmaerkteContainer() {
     ({ date }) => date < nextMidnight
     //  today + 1000 * 60 * 60 * 24
   );
+  const isSunday = new Date().getDay() === 0;
   return (
     <div className="rounded bg-hh-100 bg-opacity-25 w-[calc(100%-2rem)] p-1 xs:p-4 flex flex-col items-center min-h-[50vh]">
       <h1 className="text-4xl font-bold my-2 p-2 rounded text-hh-900">
@@ -38,41 +40,16 @@ export default async function FlohmaerkteContainer() {
       <div className="flex flex-col items-center gap-4 lg:gap-8 max-w-full">
         <section
           id="current-week-section"
-          className="flex flex-wrap gap-4 w-full justify-center"
+          className={`flex ${
+            isSunday ? "flex-wrap-reverse" : "flex-wrap"
+          } gap-4 w-full justify-center`}
         >
           <div
             id="heute-map-container"
             className="w-full lg:max-w-[400px] flex flex-col gap-4 items-center rounded bg-hh-200 bg-opacity-50 p-2 shadow-md"
           >
-            {!!todayFlohmaerkte.length && (
-              <div className="relative flex justify-center flex-col rounded-md bg-hh-200 w-full">
-                <h2 className="text-2xl font-semibold text-hh-800 text-center p-4  self-start">
-                  Heute
-                </h2>
-
-                <ScrollableContainer>
-                  {todayFlohmaerkte.map((floh) => (
-                    <article
-                      key={floh.id}
-                      className="relative flex flex-col items-center overflow-hidden h-[275px] min-w-[180px] gap-1"
-                    >
-                      <h3 className="text-hh-600 text-center h-[20px] w-full font-semibold text-sm truncate-1">
-                        {floh.stadtteil}
-                      </h3>
-                      <div className="overflow-hidden h-[250px] min-w-[180px] bg-white">
-                        <FlohmarktPoster
-                          key={floh.id}
-                          title={floh.title}
-                          bezirk={floh.bezirk}
-                          date={floh.date}
-                          image={floh.image}
-                          id={floh.id}
-                        />
-                      </div>
-                    </article>
-                  ))}
-                </ScrollableContainer>
-              </div>
+            {!!todayFlohmaerkte.length && !isSunday && (
+              <TodaysFlohmaerkte todayFlohmaerkte={todayFlohmaerkte} />
             )}
             <FlohmaerkteMap
               displayList={false}
@@ -83,8 +60,10 @@ export default async function FlohmaerkteContainer() {
             ></FlohmaerkteMap>
           </div>
           <div className="flex flex-grow w-full sm:min-w-[400px] justify-center sm:w-1/4">
-            <ScrollableFlohmaerkte
-              title={`Diese Woche gibt es ${thisWeekFlohmaerkte.length} ${
+            <BezirkeScrollableFlohmaerkte
+              title={`${isSunday ? "Heute" : "Diese Woche"} gibt es ${
+                thisWeekFlohmaerkte.length
+              } ${
                 thisWeekFlohmaerkte.length === 1 ? "Flohmarkt" : "Flohmärkte"
               }`}
               flohmaerkte={thisWeekFlohmaerkte}
