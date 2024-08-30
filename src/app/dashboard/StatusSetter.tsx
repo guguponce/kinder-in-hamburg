10276d7052a04a0cbbdf5185a96a6a8d503a4088
guguponce <augustoponceph@gmail.com>
2024-08-30 17:17:41 +0200

    StatusSetter style refactored

--- src/app/dashboard/StatusSetter.tsx
+++ src/app/dashboard/StatusSetter.tsx
@@ -24,36 +24,46 @@ export default function StatusSetter({
 
   return (
     <>
-      <select
-        name="status-select"
-        id="status-select"
-        className="rounded w-full"
-        value={currentStatus}
-        onChange={(e) =>
-          setCurrentStatus(
-            e.target.value as "pending" | "approved" | "rejected"
-          )
-        }
-      >
-        <option value="approved">Approved</option>
-        <option value="pending">Pending</option>
-        <option value="rejected">Rejected</option>
-      </select>
-      <button
-        className="p-2 rounded bg-hh-700 hover:bg-hh-800 text-white w-fit"
-        onClick={handleSetStatus}
-      >
-        Set Status {'"'}
-        {currentStatus}
-        {'"'}
-      </button>
-      <DeleteButton
-        id={target.id}
-        title={target.title}
-        deleteFrom="all"
-        type={type}
-        callbackURL="/dashboard/posts"
-      />
+      <div className="flex-grow h-full flex flex-wrap justify-around gap-4">
+        <div className="flex flex-col items-center w-full gap-4">
+          <select
+            name="status-select"
+            id="status-select"
+            className={`rounded border-2 border-hh-800 p-1 w-full max-w-[100px] ${
+              currentStatus === "approved"
+                ? "bg-positive-400"
+                : currentStatus === "rejected"
+                ? "bg-negative-400"
+                : "bg-hh-400"
+            }`}
+            value={currentStatus}
+            onChange={(e) =>
+              setCurrentStatus(
+                e.target.value as "pending" | "approved" | "rejected"
+              )
+            }
+          >
+            <option value="approved">Approved</option>
+            <option value="pending">Pending</option>
+            <option value="rejected">Rejected</option>
+          </select>
+          <button
+            className="p-2 rounded bg-hh-700 hover:bg-hh-800 text-white w-fit"
+            onClick={handleSetStatus}
+          >
+            Set Status {'"'}
+            {currentStatus}
+            {'"'}
+          </button>
+        </div>
+        <DeleteButton
+          id={target.id}
+          title={target.title}
+          deleteFrom="all"
+          type={type}
+          callbackURL="/dashboard/posts"
+        />
+      </div>
     </>
   );
 }
