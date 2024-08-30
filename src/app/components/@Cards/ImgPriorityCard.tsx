7cb70371f58925cf356756e51447f4d90d0a40a9
guguponce <augustoponceph@gmail.com>
2024-08-30 16:57:24 +0200

    ImagePriorityCard refactored
    -accepts spielgeraete, bezirk, stadtteil, children

--- src/app/components/@Cards/ImgPriorityCard.tsx
+++ src/app/components/@Cards/ImgPriorityCard.tsx
@@ -1,6 +1,7 @@
 import { iCard } from "@app/utils/types";
 import Link from "next/link";
 import React from "react";
+import SpielplatzgeraeteBackground from "../SpielplatzgeraeteBackground";
 
 export default function ImgPriorityCard({
   size,
@@ -10,6 +11,10 @@ export default function ImgPriorityCard({
   id,
   aspectRatio,
   description,
+  spielgeraete,
+  bezirk,
+  stadtteil,
+  children,
 }: iCard) {
   const classname =
     size === "small"
@@ -17,17 +22,20 @@ export default function ImgPriorityCard({
           container: "min-w-[150px] w-full max-w-[180px]",
           title: "text-md sm:text-md md:text-base",
           description: "hidden",
+          bezirk: "text-base ",
         }
       : size === "medium"
       ? {
           container: "min-w-[160px] w-full max-w-[250px]",
           title: "text-md sm:text-lg md:text-xl",
           description: "truncate-2 text-sm md:text-md",
+          bezirk: "text-sm md:text-md",
         }
       : {
           container: "min-w-[200px] w-full max-w-[300px]",
           title: "text-md sm:text-base md:text-xl",
           description: "truncate-2  text-sm sm:text-md md:text-lg",
+          bezirk: "text-sm sm:text-md md:text-lg",
         };
   return (
     <Link
@@ -38,28 +46,59 @@ export default function ImgPriorityCard({
       role="link"
       aria-label={`Explore ${title}`}
     >
-      <div className="cardImage h-2/3 w-full bg-gradient-to-b from-white to-hh-50">
-        <img
-          loading="lazy"
-          className="object-cover w-full rounded-[6px_6px_0_0] h-full flex justify-center items-center"
-          src={image}
-          alt={title}
-        />
-      </div>
-      <div className="cardContent w-full overflow-hidden flex-grow flex flex-col py-1 px-2">
-        <h2
-          className={`${classname.title} truncate-2 break-words cardTitle font-bold text-hh-950 w-full`}
-        >
-          {title}
-        </h2>
-        {description && (
-          <p
-            className={`${classname.description} text-hh-700 max-w-full break-words`}
-          >
-            {description}
-          </p>
+      <div
+        className={`${
+          image ? "h-2/3" : "h-1/2"
+        } cardImage w-full bg-gradient-to-b from-white to-hh-50`}
+      >
+        {image ? (
+          <img
+            loading="lazy"
+            className="object-cover w-full rounded-[6px_6px_0_0] h-full flex justify-center items-center"
+            src={image}
+            alt={title}
+          />
+        ) : (
+          spielgeraete && (
+            <SpielplatzgeraeteBackground
+              bgColor="#33404D"
+              color="#1F262E"
+              spList={spielgeraete}
+            />
+          )
         )}
       </div>
+      {children ? (
+        children
+      ) : (
+        <div
+          className={` ${
+            !image &&
+            "p-1 bg-gradient-to-t from-[#e3e8edb9] to-[#ACBAC810] rounded border-hh-100 hover:bg-hh-100 transition-all"
+          } cardContent w-full overflow-hidden flex-grow flex flex-col justify-between py-1 px-2  font-sans`}
+        >
+          <h2
+            className={`${classname.title} truncate-2 break-words cardTitle font-bold text-hh-900 w-full`}
+          >
+            {title}
+          </h2>
+          {description && (
+            <p
+              className={`${classname.description} text-hh-700 max-w-full break-words`}
+            >
+              {description}
+            </p>
+          )}
+          {bezirk && stadtteil && (
+            <p
+              className={`${classname.bezirk} text-hh-700 font-semibold max-w-full break-words flex flex-col items-start w-full`}
+            >
+              <span className="block w-fit">{bezirk}</span>
+              <span className="block w-fit self-end">{stadtteil}</span>
+            </p>
+          )}
+        </div>
+      )}
     </Link>
   );
 }
