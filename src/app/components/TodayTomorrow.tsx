e799ff47adab4f50153aaf3ab95013662d74ed6c
guguponce <augustoponceph@gmail.com>
2024-09-05 00:21:02 +0200

    TodayTomorrow changed to german

--- src/app/components/TodayTomorrow.tsx
+++ src/app/components/TodayTomorrow.tsx
@@ -16,7 +16,7 @@ export default function TodayTomorrow({
   return (
     <div className="flex justify-center items-center w-full bg-hh-100 bg-opacity-20 p-2 rounded-sm">
       <div className="flex-grow flex flex-col items-center w-1/2">
-        <h3 className="font-semibold">Today</h3>
+        <h3 className="font-semibold">Heute</h3>
         <div className="flex flex-col items-center">
           <WeatherIcon
             logo={WEATHER_CODES[todayCode.toString()].overallCondition}
@@ -25,9 +25,9 @@ export default function TodayTomorrow({
           <h4 className="font-semibold">{todayTemp}°</h4>
         </div>
       </div>
-      <div className="h-full w-1 bg-hh-700"></div>
+      <div className="h-full w-1 bg-hh-700 rounded"></div>
       <div className="flex-grow flex flex-col items-center w-1/2">
-        <h3 className="font-semibold">Tomorrow</h3>
+        <h3 className="font-semibold">Morgen</h3>
         <div className="flex flex-col items-center">
           <WeatherIcon
             logo={WEATHER_CODES[tomorrowCode.toString()].overallCondition}
