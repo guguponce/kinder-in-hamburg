d57045fe9858d6eca6a04f04d4f71ffbb2fe05ee
guguponce <augustoponceph@gmail.com>
2024-09-05 12:01:29 +0200

    SIHBanner size refactor

--- src/app/components/(index)/(sommerinhamburg)/SommerInHamburgBanner.tsx
+++ src/app/components/(index)/(sommerinhamburg)/SommerInHamburgBanner.tsx
@@ -23,16 +23,16 @@ export default async function SommerInHamburgBanner() {
   const planschbecken = (await getTypeSpielplaetze("planschbecken")) || [];
 
   return (
-    <section className="p-4 rounded-lg bg-gradient-to-b from-hh-600 to-hh-500 w-full flex gap-4 flex-col items-center max-w-[420px] sm:max-w-[800px] text-white shadow-lg bg-opacity-10 transition-all">
+    <section className="p-4 rounded-lg bg-gradient-to-b from-hh-600 to-hh-500 w-full flex gap-4 flex-col items-center max-w-[420px] sm:max-w-[800px] text-white shadow-xl bg-opacity-10 transition-all">
       <div className="sm:gap-2 flex flex-col sm:flex-row w-full items-stretch">
         <div className="flex flex-col items-center gap-1 flex-grow">
           <Link
             href={"/sommer-in-hamburg"}
-            className="text-3xl md:py-6 sm:py-4 text-center font-bold p-1 sm:mb-none hover:text-hh-50 hover:scale-[1.01] transition-all"
+            className="text-3xl md:py-4 sm:py-4 text-center font-bold p-1 sm:mb-none hover:text-hh-50 hover:scale-[1.01] transition-all"
           >
             Sommer in Hamburg
           </Link>
-          <p className="italic px-2">
+          <p className="italic text-sm px-2">
             Der Sommer ist fast vorbei, aber wir haben noch ein paar
             Empfehlungen für den letzten warmen Wochen!
           </p>
@@ -48,7 +48,7 @@ export default async function SommerInHamburgBanner() {
 
       <Link
         href="/sommer-in-hamburg"
-        className="underline underline-offset-2 font-semibold px-4 text-xl text-end text-white self-end sm:block hidden hover:scale-[1.01] transition-all hover:underline-offset-4"
+        className="underline underline-offset-2 font-semibold px-4 text-end text-white self-end sm:block hidden hover:scale-[1.01] transition-all hover:underline-offset-4"
       >
         Mehr entdecken
       </Link>
