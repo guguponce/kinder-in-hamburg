78ba2cc65cef7abc6402e59d5d6e5f9bb959f540
guguponce <augustoponceph@gmail.com>
2024-09-06 13:40:59 +0200

    Homepage fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ShortAbout from "./components/@Homepage/ShortAbout";
 import FlohmaerkteContainer from "./components/FlohmaerkteContainer";
-import SommerInHamburgBanner from "./components/(index)/(sommerinhamburg)/SommerInHamburgBanner";
+import SommerInHamburgBanner from "./components/@Index/sommerinhamburg/SommerInHamburgBanner";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 export default function Home() {
   return (
     <main
       id="homepage-main"
       className="w-full flex flex-col items-center gap-4 xs:px-2"
     >
+      {/* <section className="max-w-[800px]">
+        <WeatherBox full />
+      </section> */}
       <SommerInHamburgBanner />
       <FlohmaerkteContainer />
       <ShortAbout></ShortAbout>
