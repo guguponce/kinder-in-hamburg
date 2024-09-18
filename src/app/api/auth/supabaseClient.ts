49a235fa0ad4c0a5ab1fe5bdf3ceb8bb04236d57
guguponce <augustoponceph@gmail.com>
2024-09-18 15:04:28 +0200

    next-auth uninstalled

--- src/app/api/auth/supabaseClient.ts
+++ src/app/api/auth/supabaseClient.ts
@@ -10,14 +10,20 @@ export const createClient = () => {
 export const getClientSession = async () => {
   const supabase = createClient();
   const { data: session, error } = await supabase.auth.getSession();
-  if (error) console.log("getClientSession", error, "getClientSession");
+  if (error) {
+    if ("Auth session missing!" === error.message) return { user: null };
+    console.log("getClientSession", error, "getClientSession");
+  }
   return session;
 };
 
 export const getClientUser = async () => {
   const supabase = createClient();
   const { data: session, error } = await supabase.auth.getUser();
-  if (error) console.log("getClientUser", error, "getClientUser");
+  if (error) {
+    if ("Auth session missing!" === error.message) return { user: null };
+    console.log("getClientUser", error, "getClientUser");
+  }
 
   return session;
 };
