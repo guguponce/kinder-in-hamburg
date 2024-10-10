6a19a78b3eef4042f9331439ccdf4180fcfd1f74
guguponce <augustoponceph@gmail.com>
2024-10-10 11:54:15 +0200

    revalidateAndRoute deleted

--- src/app/utils/actions/revalidate.ts
+++ src/app/utils/actions/revalidate.ts
@@ -1,8 +1,6 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
-import { sleep } from "../functions";
-import { redirect } from "next/navigation";
 
 export async function revalidatePost() {
   revalidatePath("/", "layout");
@@ -18,12 +16,4 @@ export async function revalidateFlohmarkt() {
   revalidatePath("/update-flohmarkt/", "layout");
   revalidatePath("/flohmarkt-approval/", "layout");
   revalidatePath("/update-suggested-flohmarkt/", "layout");
-}
-
-export async function revalidateAndRoute(id: string) {
-  revalidatePost().then(() => {
-    sleep(2000).then(() => {
-      redirect(`/posts/${id}`);
-    });
-  });
 }
