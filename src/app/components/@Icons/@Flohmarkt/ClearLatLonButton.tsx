bb572db7d84d52b94aae1c0bedf4cf008ed6efed
guguponce <augustoponceph@gmail.com>
2024-10-10 12:15:16 +0200

    ClearLatLonButton refactored to accept flohmarkt|post|spielplatz

--- src/app/components/@Icons/@Flohmarkt/ClearLatLonButton.tsx
+++ src/app/components/@Icons/@Flohmarkt/ClearLatLonButton.tsx
@@ -1,13 +1,25 @@
 "use client";
-import { clearLatLonFromFlohmarkt } from "@app/api/dbActions";
+import {
+  clearLatLonFromFlohmarkt,
+  clearLatLonFromPost,
+} from "@app/api/dbActions";
+import { clearLatLonFromSpielplatz } from "@app/api/spActions";
 import React from "react";
 
-export default function ClearLatLonButton({ id }: { id: string }) {
+export default function ClearLatLonButton({
+  type,
+  id,
+}: {
+  type: "flohmarkt" | "post" | "spielplatz";
+  id: string;
+}) {
   return (
     <button
       className="p-2 font-semibold text-white bg-negative-600 rounded-md hover:bg-negative-700"
       onClick={() => {
-        clearLatLonFromFlohmarkt(id);
+        if (type === "flohmarkt") clearLatLonFromFlohmarkt(id);
+        if (type === "post") clearLatLonFromPost(id);
+        if (type === "spielplatz") clearLatLonFromSpielplatz(id);
       }}
     >
       Clear Lat/Lon
