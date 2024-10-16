171d471bdb404ed0d027ffead75a3e6f35dde840
guguponce <augustoponceph@gmail.com>
2024-10-16 11:29:17 +0200

    Refactor supabaseAuth to handle missing user session error

--- src/app/api/auth/supabaseAuth.ts
+++ src/app/api/auth/supabaseAuth.ts
@@ -2,6 +2,7 @@
 import { headers } from "next/headers";
 import { createClient } from "@auth/server";
 import { redirect } from "next/navigation";
+import { iUserMetadata } from "./types";
 
 export const signIn = async () => {
   const supabase = createClient();
@@ -30,10 +31,12 @@ export const getServerSession = async () => {
 
 export const getServerUser = async () => {
   const supabase = createClient();
-  const { data: user, error } = await supabase.auth.getUser();
+  const { data: userData, error } = await supabase.auth.getUser();
   if (error) {
-    if ("Auth session missing!" === error.message) return { user: null };
+    if ("Auth session missing!" === error.message) return null;
     const session = await getServerSession();
   }
-  return user;
+  const { user } = userData;
+  if (!user) return null;
+  return user.user_metadata as iUserMetadata;
 };
