d65b8c6371d178a87608084ed2a8491dafa9b5c9
guguponce <augustoponceph@gmail.com>
2024-10-23 00:28:06 +0200

    ShortAbout component in other folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import ShortAbout from "./components/@Homepage/ShortAbout";
+import ShortAbout from "@components/@Index/ShortAbout";
 import FlohmaerkteContainer from "./components/FlohmaerkteContainer";
-import SommerInHamburgBanner from "./components/@Index/sommerinhamburg/SommerInHamburgBanner";
 
 export default function Home() {
   return (
