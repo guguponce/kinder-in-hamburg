dce3d17760c4d7409d08a70ab4b8f4e78c5239de
guguponce <augustoponceph@gmail.com>
2024-10-25 16:52:54 +0200

    Title fixed Laternenumzuege

--- src/app/(events)/laternenumzuege/page.tsx
+++ src/app/(events)/laternenumzuege/page.tsx
@@ -5,7 +5,6 @@ import AdminRoute from "@app/providers/AdminRoute";
 import React from "react";
 import { getTodayNexMonday } from "@app/utils/functions";
 import dynamic from "next/dynamic";
-import laterneImg from "./laterne.svg";
 
 const DynamicEventsMap = dynamic(
   () => import("../../components/@Map/DynamicEventsMap"),
@@ -32,12 +31,12 @@ export default async function LaternenumzuegePage() {
       <main className="flex flex-col w-full max-w-[800px] p-1">
         <section className="p-4 rounded-lg bg-gradient-to-b from-hh-950 to-hh-800 w-full flex gap-4 flex-col items-center sm:max-w-[800px] text-white shadow-xl bg-opacity-10 transition-all">
           <div className="w-full flex gap-2 justify-between items-stretch">
-            <div className="h-10 laterneWalkerIcon aspect-square "></div>
+            {/* <div className="h-10 laterneWalkerIcon aspect-square "></div> */}
             <h1 className="text-3xl flex-grow font-bold ">
               Laternenumzüge in Hamburg
             </h1>
           </div>
-          <div className="h-10 laterneWalkerIcon aspect-square "></div>
+          {/* <div className="h-10 laterneWalkerIcon aspect-square "></div> */}
 
           <p className="italic">
             Eine der bekanntesten Herbsttraditionen bringt Familien und Freunde
