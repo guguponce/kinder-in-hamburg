45f42a692fd2838361431c814408acf613ef19d3
guguponce <augustoponceph@gmail.com>
2024-10-25 17:09:17 +0200

    Light from banner image fixed

--- src/app/components/@Index/laternenumzug/LaterneImage.tsx
+++ src/app/components/@Index/laternenumzug/LaterneImage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function LaterneImage() {
   return (
     <div className="w-full h-full relative">
-      <div className="absolute w-16 h-16 bottom-[5%] left-[1%] bg-gradient-radial blur-[8px] from-orange-300 to-orange-400 rounded-full"></div>
+      <div className="absolute w-16 h-16 left-[-8%] bottom-[2%] sm:bottom-[5%] sm:left-[-3%] bg-gradient-radial blur-[8px] from-orange-300 to-orange-400 rounded-full"></div>
       <Image
         src="/assets/icons/laterne/laterne.svg"
         alt="Laternen"
