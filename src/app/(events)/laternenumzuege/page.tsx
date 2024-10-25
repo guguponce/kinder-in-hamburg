381495ab2bfce64646366eb2f2659a0254e97f8b
guguponce <augustoponceph@gmail.com>
2024-10-25 16:36:30 +0200

    Laterneumzuege page created

---
+++ src/app/(events)/laternenumzuege/page.tsx
@@ -0,0 +1,74 @@
+import { getAllEventsFromType } from "@app/api/dbActions";
+import NotFound from "@app/components/@NotFound/NotFound";
+import BezirkableEventsList from "@app/components/BezirkableEventsList";
+import AdminRoute from "@app/providers/AdminRoute";
+import React from "react";
+import { getTodayNexMonday } from "@app/utils/functions";
+import dynamic from "next/dynamic";
+import laterneImg from "./laterne.svg";
+
+const DynamicEventsMap = dynamic(
+  () => import("../../components/@Map/DynamicEventsMap"),
+  {
+    ssr: false,
+    loading: () => (
+      <article className="w-full max-w-[800px] aspect-square sm:aspect-video max-h-[60vh]">
+        <img
+          src="/assets/bezirke/hamburg.webp"
+          alt="Hamburg"
+          className="w-full h-full object-cover"
+        />
+      </article>
+    ),
+  }
+);
+export default async function LaternenumzuegePage() {
+  const laternenEvents = await getAllEventsFromType("laterne");
+  if (!laternenEvents) return <NotFound multiples type="event" />;
+  if (!laternenEvents.length) return;
+  const { today, nextMonday } = getTodayNexMonday();
+  return (
+    <AdminRoute>
+      <main className="flex flex-col w-full max-w-[800px] p-1">
+        <section className="p-4 rounded-lg bg-gradient-to-b from-hh-950 to-hh-800 w-full flex gap-4 flex-col items-center sm:max-w-[800px] text-white shadow-xl bg-opacity-10 transition-all">
+          <div className="w-full flex gap-2 justify-between items-stretch">
+            <div className="h-10 laterneWalkerIcon aspect-square "></div>
+            <h1 className="text-3xl flex-grow font-bold ">
+              Laternenumzüge in Hamburg
+            </h1>
+          </div>
+          <div className="h-10 laterneWalkerIcon aspect-square "></div>
+
+          <p className="italic">
+            Eine der bekanntesten Herbsttraditionen bringt Familien und Freunde
+            zusammen, die mit bunten Laternen durch die Straßen ziehen und dabei
+            Lieder singen. Diese Umzüge, oft zu Ehren des Heiligen Sankt Martin,
+            sind in Hamburg in vielen Stadtteilen vertreten – von kleinen
+            Nachbarschaftsumzügen bis zu großen öffentlichen Veranstaltungen.
+            Hier haben wir eine Übersicht der kommenden Termine für euch
+            zusammengestellt, damit ihr die herbstlichen Abende in Hamburg
+            gemeinsam erleben könnt.
+          </p>
+          <section className="self-start max-w-full">
+            <BezirkableEventsList
+              variant="transparent-dark"
+              eventsList={laternenEvents}
+            />
+          </section>
+          <article className="md:p-4">
+            <DynamicEventsMap
+              darkBackground
+              thisWeek={laternenEvents.filter(
+                (event) =>
+                  event.date > today - 1000 * 60 * 60 * 24 &&
+                  event.date < nextMonday
+              )}
+              future={laternenEvents.filter((event) => event.date > nextMonday)}
+              today={today}
+            />
+          </article>
+        </section>
+      </main>
+    </AdminRoute>
+  );
+}
