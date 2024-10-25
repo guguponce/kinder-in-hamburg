d8da76066c92a30aae306cb87640bc00e8511377
guguponce <augustoponceph@gmail.com>
2024-10-25 16:51:37 +0200

    LaternenBanner in Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ShortAbout from "@components/@Index/ShortAbout";
 import FlohmaerkteContainer from "./components/FlohmaerkteContainer";
+import LaternenBanner from "./components/@Index/laternenumzug/LaternenBanner";
 
 export default function Home() {
   return (
@@ -8,7 +9,7 @@ export default function Home() {
       id="homepage-main"
       className="w-full flex flex-col items-center gap-4 xs:px-2"
     >
-      {/* <SommerInHamburgBanner /> */}
+      <LaternenBanner />
       <FlohmaerkteContainer />
       <ShortAbout></ShortAbout>
     </main>
