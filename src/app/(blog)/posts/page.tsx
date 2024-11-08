52c0bcc332ad903cc2050bd976cf2d5ea589fe2c
guguponce <augustoponceph@gmail.com>
2024-11-08 11:35:32 +0100

    feat: implement caching for approved posts retrieval and update component to use URLFilteredList

--- src/app/(blog)/posts/page.tsx
+++ src/app/(blog)/posts/page.tsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { getAllApprovedPosts } from "@app/api/dbActions";
-import FilterablePostList from "@app/components/FilterablePostList";
 import AdminRoute from "@app/providers/AdminRoute";
+import URLFilteredList from "./URLFilteredList";
+import { getAllApprovedPosts } from "@app/api/dbActions";
+import { unstable_cache } from "next/cache";
 
+export const revalidate = 20;
+
+const cachedPosts = unstable_cache(getAllApprovedPosts, ["allApprovedPosts"], {
+  revalidate: 300,
+});
 export default async function PostsPage() {
-  const postsList = await getAllApprovedPosts();
+  // -----------------------------------------------
+  const postsList = await cachedPosts();
   if (!postsList) return <div>There was a problem retrieving posts</div>;
   return (
     <AdminRoute>
       <main className="w-full max-w-[1000px]">
-        <FilterablePostList postsList={postsList}>
+        <URLFilteredList postsList={postsList}>
           <h1 className="font-bold min-w-fit text-center   text-3xl ">
             All posts
           </h1>
-        </FilterablePostList>
+        </URLFilteredList>
       </main>
     </AdminRoute>
   );
