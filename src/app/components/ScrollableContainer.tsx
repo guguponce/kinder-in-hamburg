4d608df65fb1973cae145511446a08c1887f6e0b
guguponce <augustoponceph@gmail.com>
2024-11-13 13:29:22 +0100

    feat: add customizable scrollbar color to ScrollableContainer component

--- src/app/components/ScrollableContainer.tsx
+++ src/app/components/ScrollableContainer.tsx
@@ -3,10 +3,13 @@ import React from "react";
 export default function ScrollableContainer({
   children,
   vertical = false,
+  color = "500",
 }: {
+  color?: "300" | "500" | "800";
   vertical?: boolean;
   children: React.ReactNode;
 }) {
+  const scrollbarThumbColor = `scrollbarThumb-${color}`;
   return (
     <div
       className={`flex items-center ${
@@ -17,8 +20,8 @@ export default function ScrollableContainer({
         className={`${
           vertical
             ? "verticalScrollbar w-full overflow-y-auto h-fit max-h-full flex-col"
-            : "horizontalScrollbar overflow-x-auto w-fit max-w-full p-1 pb-4 pt-2  h-full"
-        }  flex gap-2 lg:gap-4 items-stretch`}
+            : "horizontalScrollbar overflow-x-auto w-fit max-w-full p-1 pb-4 pt-2 h-full"
+        } ${scrollbarThumbColor} flex gap-2 lg:gap-4 items-stretch`}
       >
         {children}
       </div>
