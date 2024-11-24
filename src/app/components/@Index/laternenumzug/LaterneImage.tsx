b92b8394d2c5f3df272b75fe2de23c22de43a888
guguponce <augustoponceph@gmail.com>
2024-11-24 02:09:21 +0100

    feat: update LaterneImage component for improved animation and size

--- src/app/components/@Index/laternenumzug/LaterneImage.tsx
+++ src/app/components/@Index/laternenumzug/LaterneImage.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function LaterneImage() {
   return (
     <>
-      <div className="absolute w-16 h-16 left-[-8%] bottom-[2%] sm:bottom-[5%] sm:left-[-8%] bg-gradient-radial blur-[8px] from-orange-300 to-orange-400 rounded-full"></div>
+      <div className="absolute animate-pulse w-4 h-4 left-[-8%] bottom-[2%] sm:bottom-[5%] sm:left-[-4%] bg-gradient-radial blur-[4px] from-orange-300 to-orange-400 rounded-full"></div>
       <Image
         src="/assets/icons/laterne/laterne.svg"
         alt="Laternen"
