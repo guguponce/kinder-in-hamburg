20f5f8140645c73af4969c225a5e4e7665e1f120
guguponce <augustoponceph@gmail.com>
2024-11-24 02:32:33 +0100

    feat: add WeihnachtsBanner component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ShortAbout from "@components/@Index/ShortAbout";
 import FlohmaerkteContainer from "./components/FlohmaerkteContainer";
 import LaternenBanner from "./components/@Index/laternenumzug/LaternenBanner";
+import WeihnachtsBanner from "./components/@Index/WeihnachtsBanner";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       id="homepage-main"
       className="w-full flex flex-col items-center gap-4 xs:px-2"
     >
+      <WeihnachtsBanner />
       <LaternenBanner />
       <FlohmaerkteContainer />
       <ShortAbout></ShortAbout>
