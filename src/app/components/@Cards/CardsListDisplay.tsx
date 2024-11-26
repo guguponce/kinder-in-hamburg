4356e5f9b076b4593045ca01368e71999e2db434
guguponce <augustoponceph@gmail.com>
2024-11-26 10:58:32 +0100

    CardsListDisplay changed flex for grid

--- src/app/components/@Cards/CardsListDisplay.tsx
+++ src/app/components/@Cards/CardsListDisplay.tsx
@@ -11,7 +11,7 @@ export default function CardsListDisplay({
 }) {
   return (
     <div
-      className={`gap-2 lg:gap-8 flex md:flex-row justify-center items-start flex-row flex-wrap min-h-[350px] h-full overflow-auto p-2 w-full min-w-fit`}
+      className={`gap-2 lg:gap-8 min-h-[350px] h-full overflow-auto p-2 w-full grid grid-cols-[repeat(auto-fit,minmax(180px,1fr))] justify-items-center`}
     >
       {cardPosts.map(({ id, title, text, image, categories }, index) => {
         return (
@@ -25,8 +25,8 @@ export default function CardsListDisplay({
                 !!image?.length
                   ? image[0]
                   : categories.includes("Badeplatz")
-                  ? "/assets/icons/swim.svg"
-                  : "/assets/icons/spielplatz/spielplatz.svg"
+                    ? "/assets/icons/swim.svg"
+                    : "/assets/icons/spielplatz/spielplatz.svg"
               }
               description={getPlainText(text)}
             />
