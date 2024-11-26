0ff5c3d160723335464f53349a8cf7b959231f75
guguponce <augustoponceph@gmail.com>
2024-11-26 10:58:07 +0100

    fix: enhance ImgPriorityCard layout and styling for better responsiveness

--- src/app/components/@Cards/ImgPriorityCard.tsx
+++ src/app/components/@Cards/ImgPriorityCard.tsx
@@ -25,18 +25,18 @@ export default function ImgPriorityCard({
           bezirk: "text-base ",
         }
       : size === "medium"
-      ? {
-          container: "min-w-[160px] w-full max-w-[250px]",
-          title: "text-md sm:text-lg md:text-xl",
-          description: "truncate-2 text-sm md:text-md",
-          bezirk: "text-sm md:text-md",
-        }
-      : {
-          container: "min-w-[200px] w-full max-w-[300px]",
-          title: "text-md sm:text-base md:text-xl",
-          description: "truncate-2  text-sm sm:text-md md:text-lg",
-          bezirk: "text-sm sm:text-md md:text-lg",
-        };
+        ? {
+            container: "min-w-[160px] w-full max-w-[250px]",
+            title: "text-md sm:text-lg md:text-xl",
+            description: "truncate-2 text-sm italic",
+            bezirk: "text-sm md:text-md text-hh-900",
+          }
+        : {
+            container: "min-w-[200px] w-full max-w-[300px]",
+            title: "text-md sm:text-base md:text-xl",
+            description: "truncate-2  text-sm sm:text-base italic",
+            bezirk: "text-sm sm:text-md md:text-lg text-hh-900",
+          };
   return (
     <Link
       href={link || `/posts/${id}`}
@@ -48,7 +48,7 @@ export default function ImgPriorityCard({
     >
       <div
         className={`${
-          image ? "h-2/3" : "h-1/2"
+          children ? "flex-grow" : image ? "h-2/3" : "h-1/2"
         } cardImage w-full bg-gradient-to-b from-white to-hh-50`}
       >
         {image ? (
@@ -75,7 +75,7 @@ export default function ImgPriorityCard({
           className={` ${
             !image &&
             "p-1 bg-gradient-to-t from-[#e3e8edb9] to-[#ACBAC810] rounded border-hh-100 hover:bg-hh-100 transition-all"
-          } cardContent w-full overflow-hidden flex-grow flex flex-col justify-between py-1 px-2  font-sans`}
+          } cardContent w-full overflow-hidden flex-grow flex flex-col justify-between py-1 px-2 font-sans min-h-fit`}
         >
           <h2
             className={`${classname.title} truncate-3 break-words cardTitle font-bold text-hh-900 w-full`}
@@ -89,14 +89,17 @@ export default function ImgPriorityCard({
               {description}
             </p>
           )}
-          {bezirk && stadtteil && (
-            <p
-              className={`${classname.bezirk} text-hh-700 font-semibold max-w-full break-words flex flex-col items-start w-full`}
-            >
-              <span className="block w-fit">{bezirk}</span>
-              <span className="block w-fit self-end">{stadtteil}</span>
-            </p>
-          )}
+          {bezirk ||
+            (stadtteil && (
+              <p
+                className={`${classname.bezirk} text-hh-700 font-semibold max-w-full break-words flex flex-col items-start w-full`}
+              >
+                {bezirk && <span className="block w-fit">{bezirk}</span>}
+                {stadtteil && (
+                  <span className="block w-fit self-end">{stadtteil}</span>
+                )}
+              </p>
+            ))}
         </div>
       )}
     </Link>
