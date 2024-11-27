2bc352f7add7d693697e90454dd95be77cff446d
guguponce <augustoponceph@gmail.com>
2024-11-27 14:27:57 +0100

    refactor: update EventsGallery to fetch events for the current week and sort them by date

--- src/app/components/@Index/laternenumzug/EventsGallery.tsx
+++ src/app/components/@Index/laternenumzug/EventsGallery.tsx
@@ -1,27 +1,40 @@
 import {
+  getAllEventsThisWeek,
   getAllFutureEventsFromType,
   getFutureApprovedEventsFromType,
 } from "@app/api/dbActions";
 import React from "react";
 import ClientEventsGallery from "./ClientEventsGallery";
-import { iEventType } from "@app/utils/types";
+import { iEventType, iFlohmarkt } from "@app/utils/types";
+import { getTodayNexMonday } from "@app/utils/functions";
 
 export default async function EventsGallery({
   eventType,
 }: {
   eventType: iEventType;
 }) {
-  // ---------------------
-  const eventsList =
-    // (eventType === "weihnachtsmarkt"
-    //   ? await getAllFutureEventsFromType("weihnachtsmarkt")
-    //   :
-    (await getFutureApprovedEventsFromType(eventType)) || [];
-  const extra =
-    eventType === "weihnachtsmarkt"
-      ? (await getFutureApprovedEventsFromType("adventsevent")) || []
-      : [];
-  if (eventsList.length === 0) return null;
+  const eventTypes: iEventType[] =
+    eventType === "adventsevent"
+      ? ["adventsevent", "weihnachtsmarkt"]
+      : eventType === "laterne"
+        ? ["laternewerkstatt", "laterne"]
+        : [eventType];
+  const eventsList = (await getAllEventsThisWeek(eventTypes)) || [];
+  const today = getTodayNexMonday().today - 1000 * 60 * 60 * 1;
+  const sortedList = [...eventsList]
+    .reduce(
+      (acc, event) => {
+        if (event.date >= today && event.type !== "weihnachtsmarkt") {
+          acc[0].push(event);
+          return acc;
+        }
+        acc[1].push(event);
+        return acc;
+      },
+      [[], []] as [iFlohmarkt[], iFlohmarkt[]]
+    )
+    .flat();
+  if (sortedList.length === 0) return null;
 
-  return <ClientEventsGallery eventsList={[...extra, ...eventsList]} />;
+  return <ClientEventsGallery eventsList={sortedList} />;
 }
