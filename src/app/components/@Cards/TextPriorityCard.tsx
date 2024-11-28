43f334230b332a808bba3b2e3e12b67ad3272486
guguponce <augustoponceph@gmail.com>
2024-11-28 15:09:55 +0100

    refactor: enhance TextPriorityCard with imgClassname and cardContainerClassname for improved styling flexibility

--- src/app/components/@Cards/TextPriorityCard.tsx
+++ src/app/components/@Cards/TextPriorityCard.tsx
@@ -2,38 +2,43 @@ import { iCard } from "@app/utils/types";
 import Link from "next/link";
 import React from "react";
 import CardLogo from "../@Icons/CardLogo";
+import { cn } from "@app/utils/functions";
 
 export default function TextPriorityCard({
   id,
   title,
   image,
   description,
-  categories = "Indoor",
   link,
   size,
+  imgClassname,
+  cardContainerClassname,
 }: iCard) {
   const classname =
     size === "small"
       ? {
           container: "min-w-[150px] max-w-[180px]",
-          description: "truncate-3  text-xs",
-          title: "text-sm sm:text-md md:text-base",
+          description: "truncate-3 text-xs",
+          title: "text-sm",
         }
       : size === "medium"
-      ? {
-          container: "min-w-[160px] max-w-[250px]",
-          description: "truncate-5  text-xs",
-          title: "text-md sm:text-lg md:text-xl",
-        }
-      : {
-          container: "min-w-[200px] max-w-[300px]",
-          description: "truncate-5  text-sm",
-          title: "text-md sm:text-base md:text-xl",
-        };
+        ? {
+            container: "min-w-[160px] max-w-[250px]",
+            description: "truncate-5  text-xs",
+            title: "text-md sm:text-lg md:text-xl",
+          }
+        : {
+            container: "min-w-[200px] max-w-[300px]",
+            description: "truncate-5  text-sm",
+            title: "text-md sm:text-base md:text-xl",
+          };
   return (
     <Link
       href={link || `/posts/${id}`}
-      className={`${classname.container} w-1/2 sm:w-1/3 md:w-1/4 singleCard aspect-[0.66] rounded-md shadow-md hover:shadow-lg flex flex-col items-center  bg-hh-50 overflow-hidden`}
+      className={cn(
+        `${classname.container} w-1/2 sm:w-1/3 md:w-1/4 singleCard aspect-[0.66] rounded-md shadow-md hover:shadow-lg flex flex-col items-center  bg-hh-50 overflow-hidden`,
+        cardContainerClassname
+      )}
       role="link"
       aria-label={`Explore ${title}`}
     >
@@ -41,7 +46,7 @@ export default function TextPriorityCard({
         {!!image ? (
           <img
             loading="lazy"
-            className="object-cover w-full h-full"
+            className={cn("object-cover w-full h-full", imgClassname)}
             src={image}
             alt={title}
           />
@@ -50,12 +55,14 @@ export default function TextPriorityCard({
         )}
       </div>
       <div className="cardContent w-full overflow-hidden flex-grow flex flex-col p-2">
-        <h2 className="truncate-2 break-words cardTitle font-bold text-sm sm:text-md md:text-lg text-hh-950  break-word max-w-full">
+        <h2
+          className={`truncate-2 break-words cardTitle font-bold ${classname.title} text-hh-950 break-word max-w-full`}
+        >
           {title}
         </h2>
         {description && (
           <p
-            className={`${classname.description} text-hh-700 italic max-w-full break-words`}
+            className={`${classname.description} flex-grow flex flex-col justify-center text-hh-700 italic max-w-full break-words`}
           >
             {description}
           </p>
