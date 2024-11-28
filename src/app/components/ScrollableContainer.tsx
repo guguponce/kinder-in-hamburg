19c6bc01dfdd3ad10ab8448291c20f98d995689c
guguponce <augustoponceph@gmail.com>
2024-11-28 15:11:29 +0100

    refactor: enhance ScrollableContainer with boxStyle prop for customizable styling

--- src/app/components/ScrollableContainer.tsx
+++ src/app/components/ScrollableContainer.tsx
@@ -1,13 +1,16 @@
+import { cn } from "@app/utils/functions";
 import React from "react";
 
 export default function ScrollableContainer({
   children,
   vertical = false,
   color = "500",
+  boxStyle,
 }: {
   color?: "300" | "500" | "800";
   vertical?: boolean;
   children: React.ReactNode;
+  boxStyle?: string;
 }) {
   const scrollbarThumbColor = `scrollbarThumb${color}`;
   return (
@@ -17,11 +20,14 @@ export default function ScrollableContainer({
       } overflow-hidden h-full`}
     >
       <div
-        className={`${
-          vertical
-            ? "verticalScrollbar w-full overflow-y-auto h-fit max-h-full flex-col"
-            : "horizontalScrollbar overflow-x-auto w-fit max-w-full p-1 pb-4 pt-2 h-full"
-        } ${scrollbarThumbColor} flex gap-2 lg:gap-4 items-stretch`}
+        className={cn(
+          `${
+            vertical
+              ? "verticalScrollbar w-full overflow-y-auto h-fit max-h-full flex-col"
+              : "horizontalScrollbar overflow-x-auto w-fit max-w-full p-1 pb-4 pt-2 h-full"
+          } ${scrollbarThumbColor} flex gap-2 lg:gap-4 items-stretch`,
+          boxStyle
+        )}
       >
         {children}
       </div>
