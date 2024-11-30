5f3aeca6fb9d8f3ea114e59dba535cb8d6c6ba03
guguponce <augustoponceph@gmail.com>
2024-12-01 00:20:35 +0100

    feat: update TodaysFlohmaerkte component for improved styling and layout

--- src/app/components/TodaysFlohmaerkte.tsx
+++ src/app/components/TodaysFlohmaerkte.tsx
@@ -9,18 +9,18 @@ export default function TodaysFlohmaerkte({
   todayFlohmaerkte: iFlohmarkt[];
 }) {
   return (
-    <div className="relative flex justify-center flex-col rounded-md bg-hh-200 min-w-[220px] w-fit max-w-full p-2 pt-0 shadow-sm">
+    <div className="relative flex  flex-col rounded-md bg-hh-200 bg-opacity-50 min-w-[220px] w-fit max-w-full p-2 pt-0 shadow-sm h-fit">
       <h2 className="text-2xl font-semibold text-hh-800 text-center p-2 pb-0  self-start">
         Heute
       </h2>
 
-      <ScrollableContainer>
+      <ScrollableContainer containerStyle=" self-center h-fit" color="800">
         {todayFlohmaerkte.map((floh) => (
           <article
             key={floh.id}
             className="relative flex flex-col items-center overflow-hidden h-[275px] min-w-[180px] gap-1"
           >
-            <h3 className="text-hh-600 text-center h-[20px] w-full font-semibold text-sm truncate-1">
+            <h3 className="text-hh-800 text-center h-[20px] w-full font-semibold text-sm truncate-1">
               {floh.stadtteil}
             </h3>
             <div className="overflow-hidden h-[250px] min-w-[180px] bg-white">
