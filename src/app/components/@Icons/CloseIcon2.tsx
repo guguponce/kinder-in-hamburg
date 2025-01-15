1465c8806e85ba332b1004c5759bf3d191a9f30e
guguponce <augustoponceph@gmail.com>
2025-01-15 13:48:29 +0100

    fix: update stroke color property in CloseIcon2 component

--- src/app/components/@Icons/CloseIcon2.tsx
+++ src/app/components/@Icons/CloseIcon2.tsx
@@ -19,13 +19,13 @@ export default function CloseIcon2({
       transform={`rotate(${rotate})`}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
+      stroke={color}
       fill="none"
     >
       <g>
         <g>
           <path
             d="M16 16L12 12M12 12L8 8M12 12L16 8M12 12L8 16"
-            stroke={color}
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
