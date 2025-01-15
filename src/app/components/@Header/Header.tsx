59047219edbc04dcfc0031c076ebb6f7b6b942e7
guguponce <augustoponceph@gmail.com>
2025-01-15 13:49:26 +0100

    fix: update Header component to use Link for main logo and simplify structure for tab

--- src/app/components/@Header/Header.tsx
+++ src/app/components/@Header/Header.tsx
@@ -16,28 +16,21 @@ export default async function Header() {
       <AdminServerComponent>
         <BurgerMenu />
       </AdminServerComponent>
-      <div
-        className="h-full px-4 flex-grow rounded-sm flex items-center justify-center lg:justify-start gap-4"
-        id="logo-text"
+      <Link
+        href={"/"}
+        className="h-full overflow-hidden px-4 flex-grow rounded-sm flex items-center justify-center lg:justify-start gap-4"
+        id="main-logo-link"
       >
-        <div className="flex items-center justify-center" id="logo">
-          <Link href="/">
-            <PostLogo
-              logo="logo"
-              size="6.5rem"
-              color="#9ed0e6"
-              color2="#FFF9F3"
-            />
-          </Link>
-        </div>
+        {/* <div className="flex items-center justify-center focus:outline" id="logo"> */}
+        <PostLogo logo="logo" size="6.5rem" color="#9ed0e6" color2="#FFF9F3" />
+        {/* </div> */}
 
-        <Link
+        <span
           className={`${logoFont.className} text-3xl hidden sm:flex sm:text-4xl pt-1 align-top tracking-wide sm:tracking-widest text-end lg:text-center font-bold text-white items-end gap-4`}
-          href="/"
         >
           Kinder in HH
-        </Link>
-      </div>
+        </span>
+      </Link>
       <AdminServerComponent>
         <nav className="hidden font-semibold lg:flex items-center gap-4">
           {[
