1e51adeed4c35bb35c7fe3258411da206a543a6b
guguponce <augustoponceph@gmail.com>
2025-01-15 23:48:36 +0100

    style: add scroll buttons with TriangleIcon for enhanced scroll functionality in ScrollableContainer

--- src/app/components/ScrollableContainer.tsx
+++ src/app/components/ScrollableContainer.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@app/utils/functions";
 import React from "react";
+import TriangleIcon from "./@Icons/TriangleIcon";
 
 export default function ScrollableContainer({
   children,
@@ -15,16 +16,18 @@ export default function ScrollableContainer({
   containerStyle?: string;
 }) {
   const scrollbarThumbColor = `scrollbarThumb${color}`;
+  const scrollRef = React.useRef<HTMLDivElement>(null);
   return (
     <div
       className={cn(
-        `flex items-center ${
+        `relative flex items-center ${
           vertical ? "w-full" : "w-fit max-w-full"
         } overflow-hidden h-full`,
         containerStyle
       )}
     >
       <div
+        ref={scrollRef}
         className={cn(
           `${
             vertical
@@ -36,6 +39,28 @@ export default function ScrollableContainer({
       >
         {children}
       </div>
+      <button
+        className={`absolute ${vertical ? "rotate-90 right-0 hover:-right-1 top-0 hover:top-1" : "bottom-0 left-0"} rounded-[4px_0_0_4px] hover:w-4 p-[1px] bg-hh-${color} flex justify-start items-center text-hh-50  focus:outline-none focus-within:outline-none focus-visible:outline-none`}
+        onClick={() => {
+          if (scrollRef.current) {
+            if (vertical) scrollRef.current.scrollTop -= 200;
+            else scrollRef.current.scrollLeft -= 200;
+          }
+        }}
+      >
+        <TriangleIcon color={"#fefefe"} rotate={270} size="6px" />
+      </button>
+      <button
+        className={`absolute ${vertical ? "rotate-90 hover:-right-1 hover:bottom-1" : ""} rounded-[0_4px_4px_0] right-0 bottom-0  p-[1px] flex justify-end items-center bg-hh-${color} bg-opacity-75 hover:bg-opacity-100 hover:w-4 text-hh-50 focus:outline-none focus-within:outline-none focus-visible:outline-none`}
+        onClick={() => {
+          if (scrollRef.current) {
+            if (vertical) scrollRef.current.scrollTop += 200;
+            else scrollRef.current.scrollLeft += 200;
+          }
+        }}
+      >
+        <TriangleIcon color={"#fefefe"} rotate={90} size="6px" />
+      </button>
     </div>
   );
 }
