b6de0c5c4c6de1ee838a9d3fe73b39bcd7443321
guguponce <augustoponceph@gmail.com>
2025-02-20 22:22:45 +0100

    feat: add admin prop to GeneralContainer for conditional rendering of AdminServerComponent

--- src/app/components/GeneralContainer.tsx
+++ src/app/components/GeneralContainer.tsx
@@ -1,3 +1,4 @@
+import AdminServerComponent from "@app/providers/AdminServerComponents";
 import { cn } from "@app/utils/functions";
 import React from "react";
 
@@ -5,15 +6,30 @@ type GeneralContainerProps = {
   type?: "div" | "section" | "article" | "main";
   children: React.ReactNode;
   classname?: string;
+  admin?: boolean;
 };
 
 const GeneralContainer: React.FC<GeneralContainerProps> = ({
   type = "div",
   children,
   classname,
+  admin,
 }) => {
   const Tag = type; // Dynamic element assignment based on the "type" prop
-
+  if (admin) {
+    return (
+      <AdminServerComponent>
+        <Tag
+          className={cn(
+            "w-full p-2 lg:p-4 rounded lg:rounded-lg flex flex-col items-center bg-hh-200 bg-opacity-50 backdrop-blur-sm",
+            classname
+          )}
+        >
+          {children}
+        </Tag>
+      </AdminServerComponent>
+    );
+  }
   return (
     <Tag
       className={cn(
