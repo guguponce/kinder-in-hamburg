cefda3d2417cfca8db71648f2397a76cc4fba176
guguponce <augustoponceph@gmail.com>
2025-02-20 22:36:53 +0100

    URLFilteredList: conditional AlterFilterButton, Map added

--- src/app/(blog)/posts/URLFilteredList.tsx
+++ src/app/(blog)/posts/URLFilteredList.tsx
@@ -9,7 +9,6 @@ import React, {
 } from "react";
 import CardsListDisplay from "@components/@Cards/CardsListDisplay";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { bezirke as bezirkeNames, categoryNames } from "@app/utils/constants";
 import PostsFilter from "@app/(suggestions)/post-suggestion/PostFilters";
 import {
   createArrayQueryString,
@@ -21,70 +20,28 @@ import {
   filterStadtteileFX,
   orderListFx,
   removeFilter,
+  getAvailableStadtteile,
+  getAvailableBezirke,
+  getAvailableCategories,
+  orderOptionsNames,
+  orderOptions,
+  isTypeOrder,
+  orderType,
 } from "./filterFunctions";
 import { FilterButton } from "./DeleteFilters";
 import SearchInput from "./SearchInput";
 import CloseIcon2 from "@app/components/@Icons/CloseIcon2";
+import MarkersLists from "@app/components/@Map/PopUpsMarkers/MarkersLists";
+import dynamic from "next/dynamic";
 
+const DynamicGeneralMap = dynamic(
+  () => import("@app/components/@Map/GeneralMap"),
+  {
+    ssr: false,
+  }
+);
 const CardsListDisplayMemo = React.memo(CardsListDisplay);
-const getAvailableBezirke = (postsList: iPost[]) => {
-  let bezirke = new Set<iBezirk>();
-  for (let i = 0; i < postsList.length; i++) {
-    const { bezirk } = postsList[i];
-    bezirke.add(bezirk);
-    if (bezirke.size === bezirkeNames.length) break;
-  }
-  return Array.from(bezirke).sort((a, b) => a.localeCompare(b));
-};
-const getAvailableStadtteile = (postsList: iPost[], bezirke: iBezirk[]) => {
-  const bWS = postsList.reduce(
-    (acc, post) => {
-      const { stadtteil, bezirk } = post;
-      if (!bezirke.includes(bezirk) || !stadtteil) return acc;
 
-      if (!acc[bezirk]) acc[bezirk] = new Set<string>();
-
-      acc[bezirk].add(stadtteil);
-
-      return acc;
-    },
-    {} as Record<string, Set<string>>
-  );
-
-  // Convert Sets back to arrays for the final output
-  const result = Object.fromEntries(
-    Object.entries(bWS).map(([bezirk, stadtteileSet]) => [
-      bezirk,
-      Array.from(stadtteileSet),
-    ])
-  );
-
-  return result;
-};
-const getAvailableCategories = (postsList: iPost[]) => {
-  let categories = new Set<string>();
-  for (let i = 0; i < postsList.length; i++) {
-    const { categories: cats } = postsList[i];
-    for (let j = 0; j < cats.length; j++) {
-      categories.add(cats[j]);
-      if (categories.size === categoryNames.length) break;
-    }
-    if (categories.size === categoryNames.length) break;
-  }
-  return Array.from(categories).sort((a, b) => a.localeCompare(b));
-};
-type orderType = "neueste" | "beliebste" | "az" | "za";
-const orderOptionsNames = {
-  neueste: "Neueste",
-  beliebste: "Beliebteste",
-  az: "A-Z (Name)",
-  za: "Z-A (Name)",
-};
-const orderOptions: Array<orderType> = ["neueste", "beliebste", "az", "za"];
-const isTypeOrder = (type: string | null): type is orderType => {
-  if (!type) return false;
-  return type in orderOptionsNames;
-};
 export default function URLFilteredList({
   children,
   postsList,
@@ -193,7 +150,7 @@ export default function URLFilteredList({
     setQueryAlter("");
     setSearchQuery("");
     setOrder("neueste");
-    router.push(params);
+    router.push(params, { scroll: false });
   }, [params, router]);
   const anyFilterUsed = useMemo(
     () =>
@@ -204,173 +161,206 @@ export default function URLFilteredList({
     [searchQuery, bezirkeFilter, categoriesFilter, queryAlter]
   );
   if (!postsListRef) return <div>There was a problem retrieving posts</div>;
-
   return (
-    <section className="flex flex-col gap-2 w-full max-w-[1200px] bg-hh-100 rounded overflow-hidden relative min-h-[100vh]">
-      <PostsFilter
-        resetFilters={anyFilterUsed ? resetFilters : undefined}
-        params={params}
-        type={type}
-        availableCategories={availableCategories}
-        categoriesFilter={categoriesFilter}
-        setCategoriesFilter={setCategoriesFilter}
-        availableBezirke={availableBezirke}
-        bezirkeFilter={bezirkeFilter}
-        setBezirkeFilter={setBezirkeFilter}
-        availableStadtteile={availableStadtteile}
-        stadtteileFilter={stadtteileFilter}
-        setStadtteileFilter={setStadtteileFilter}
-        queryAlter={queryAlter}
-        setQueryAlter={setQueryAlter}
-        categoriesFilteringTogether={categoriesFilteringTogether}
-        setCategoriesFilteringTogether={setCategoriesFilteringTogether}
-      />
-      <aside className="relative flex flex-col-reverse gap-2 items-end sm:flex-row sm:justify-end sm:items-center  px-2 h-fit py-2">
-        <SearchInput
-          className="max-w-[600px] sm:max-w-[300px] sm:h-10  shadow focus-within:outline-hh-200 focus-within:outline-2 focus-within:outline"
-          searchQuery={searchQuery}
-          setSearchQuery={setSearchQuery}
+    <div className="flex gap-2 flex-wrap justify-center items-start w-full">
+      <section className="flex flex-col gap-2 flex-grow bg-hh-100 rounded overflow-hidden relative min-h-[100vh] py-2">
+        <PostsFilter
+          resetFilters={anyFilterUsed ? resetFilters : undefined}
+          params={params}
+          type={
+            type === "categories" || type === "bezirke"
+              ? (p as "categories" | "bezirke")
+              : undefined
+          }
+          queryFromType={p}
+          availableCategories={availableCategories}
+          categoriesFilter={categoriesFilter}
+          setCategoriesFilter={setCategoriesFilter}
+          availableBezirke={availableBezirke}
+          bezirkeFilter={bezirkeFilter}
+          setBezirkeFilter={setBezirkeFilter}
+          availableStadtteile={availableStadtteile}
+          stadtteileFilter={stadtteileFilter}
+          setStadtteileFilter={setStadtteileFilter}
+          queryAlter={queryAlter}
+          setQueryAlter={setQueryAlter}
+          categoriesFilteringTogether={categoriesFilteringTogether}
+          setCategoriesFilteringTogether={setCategoriesFilteringTogether}
         />
-        <select
-          name="order"
-          id="order-select"
-          value={order}
-          className="px-2 py-1 ml-2 rounded-md font-semibold w-fit h-10 my-1  shadow"
-          onChange={(e) => {
-            const val = e.target.value as orderType;
-            setOrder(val);
-            router.push(
-              params +
-                "?" +
-                createQueryString(
-                  "order",
-                  val,
-                  new URLSearchParams(searchParams.toString())
-                )
-            );
-          }}
-        >
-          {orderOptions.map((opt) => (
-            <option key={opt} value={opt}>
-              {orderOptionsNames[opt]}
-            </option>
-          ))}
-        </select>
-      </aside>
+        <aside className="relative flex flex-col-reverse gap-2 items-end sm:flex-row sm:justify-end sm:items-center  px-2 h-fit py-2">
+          <SearchInput
+            className="max-w-[600px] sm:max-w-[300px] sm:h-10  shadow focus-within:outline-hh-200 focus-within:outline-2 focus-within:outline"
+            searchQuery={searchQuery}
+            setSearchQuery={setSearchQuery}
+          />
+          <select
+            name="order"
+            id="order-select"
+            value={order}
+            className="px-2 py-1 ml-2 rounded-md font-semibold w-fit h-10 my-1  shadow"
+            onChange={(e) => {
+              const val = e.target.value as orderType;
+              setOrder(val);
+              router.push(
+                params +
+                  "?" +
+                  createQueryString(
+                    "order",
+                    val,
+                    new URLSearchParams(searchParams.toString())
+                  ),
+                { scroll: false }
+              );
+            }}
+          >
+            {orderOptions.map((opt) => (
+              <option key={opt} value={opt}>
+                {orderOptionsNames[opt]}
+              </option>
+            ))}
+          </select>
+        </aside>
 
-      {anyFilterUsed && (
-        <div className="flex flex-col gap-1 rounded bg-hh-300 border-hh-900 border-2 p-1 h-fit mx-2">
-          <div className="px-1 flex flex-wrap-reverse gap-2 items-center">
-            <h4 className="font-semibold">Angewendete Filter:</h4>
+        {anyFilterUsed && (
+          <div className="flex flex-col gap-1 rounded bg-hh-300 border-hh-900 border-2 p-1 h-fit mx-2">
+            <div className="px-1 flex flex-wrap-reverse gap-2 items-center">
+              <h4 className="font-semibold">Angewendete Filter:</h4>
+              <button
+                onClick={resetFilters}
+                className="deleteFilterButton p-[2px] pl-2 ml-auto bg-negative-500 text-hh-50  font-semibold hover:bg-negative-500 hover:bg-opacity-25 hover:border-negative-800 hover:text-negative-800 focus:-outline-offset-1 focus-within:-outline-offset-1 focus-visible:-outline-offset-1 active:outline-offset-1 -800 focus:outline-negative-600 focus-within:outline-negative-600 focus-visible:outline-negative-600 rounded-full text-xs flex gap items-center focus:border-transparent focus-within:border-transparent focus-visible:border-transparent transition-all"
+              >
+                <span className="hidden xs:inline pr-1">Alle</span>
+                <span>Filter zurücksetzen</span>
+                <CloseIcon2
+                  color="#f0f1f2 "
+                  size="20"
+                  className="transition-all"
+                />
+              </button>
+            </div>
+            <div className="flex flex-wrap gap-2 items-center">
+              {queryAlter && (
+                <FilterButton
+                  filterType="Alter"
+                  filterValue={queryAlter}
+                  typeOnClick={() => {
+                    removeFilter({
+                      state: queryAlter,
+                      stringStateSetter: setQueryAlter,
+                      filterType: "alter",
+                      value: "",
+                      router,
+                      params,
+                      searchParams,
+                    });
+                  }}
+                />
+              )}
+              {!!searchQuery && (
+                <FilterButton
+                  filterType="Suche"
+                  filterValue={searchQuery}
+                  typeOnClick={() => {
+                    removeFilter({
+                      state: searchQuery,
+                      stringStateSetter: setSearchQuery,
+                      filterType: "searchQuery",
+                      value: "",
+                      router,
+                      params,
+                      searchParams,
+                    });
+                  }}
+                />
+              )}
+              {!!bezirkeFilter.length &&
+                bezirkeFilter.map((bezirk) => (
+                  <React.Fragment key={bezirk}>
+                    <FilterButton
+                      filterType="Bezirk"
+                      filterValue={bezirk}
+                      typeOnClick={() => {
+                        removeFilter({
+                          state: bezirkeFilter,
+                          arrayStateSetter: setBezirkeFilter,
+                          filterType: "bezirke",
+                          value: bezirk,
+                          router,
+                          params,
+                          searchParams,
+                        });
+                      }}
+                    />
+                  </React.Fragment>
+                ))}
+              {!!stadtteileFilter.length &&
+                stadtteileFilter.map((st) => (
+                  <React.Fragment key={st}>
+                    <FilterButton
+                      filterType="Stadtteil"
+                      filterValue={st}
+                      typeOnClick={() => {
+                        removeFilter({
+                          state: stadtteileFilter,
+                          arrayStateSetter: setStadtteileFilter,
+                          filterType: "stadtteile",
+                          value: st,
+                          router,
+                          params,
+                          searchParams,
+                        });
+                      }}
+                    />
+                  </React.Fragment>
+                ))}
+              {!!categoriesFilter.length &&
+                categoriesFilter.map((bz) => (
+                  <React.Fragment key={bz}>
+                    <FilterButton
+                      filterType="Kategorie"
+                      filterValue={bz}
+                      typeOnClick={() => {
+                        removeFilter({
+                          state: categoriesFilter,
+                          arrayStateSetter: setCategoriesFilter,
+                          filterType: "categories",
+                          value: bz,
+                          router,
+                          params,
+                          searchParams,
+                        });
+                      }}
+                    />
+                  </React.Fragment>
+                ))}
+            </div>
+          </div>
+        )}
+        <article className="flex flex-col w-full flex-grow p-2 pt-0">
+          <CardsListDisplayMemo cardPosts={displayList}>
             <button
               onClick={resetFilters}
-              className="deleteFilterButton p-[2px] pl-2 ml-auto border-2 border-negative-950 text-positive-900  font-semibold hover:bg-negative-500 hover:bg-opacity-25 hover:border-negative-800 hover:text-negative-800 focus:-outline-offset-1 focus-within:-outline-offset-1 focus-visible:-outline-offset-1 active:outline-offset-1 -800 focus:outline-negative-600 focus-within:outline-negative-600 focus-visible:outline-negative-600 rounded-full text-xs flex gap items-center focus:border-transparent focus-within:border-transparent focus-visible:border-transparent"
+              className="p-2 bg-negative-500 text-hh-50 rounded bg-opacity-50 hover:bg-opacity-75 m-1"
             >
-              <span className="hidden xs:inline pr-1">Alle</span>
-              <span>Filter zurücksetzen</span>
-              <CloseIcon2 color="#44110b " size="20" />
+              Alle Filter zurücksetzen
             </button>
-          </div>
-          <div className="flex flex-wrap gap-2 items-center">
-            {!!searchQuery && (
-              <FilterButton
-                filterType="Suche"
-                filterValue={searchQuery}
-                typeOnClick={() => {
-                  removeFilter({
-                    state: searchQuery,
-                    stringStateSetter: setSearchQuery,
-                    filterType: "searchQuery",
-                    value: "",
-                    router,
-                    params,
-                    searchParams,
-                  });
-                }}
-              />
-            )}
-            {!!bezirkeFilter.length &&
-              bezirkeFilter.map((bezirk) => (
-                <React.Fragment key={bezirk}>
-                  <FilterButton
-                    filterType="Bezirk"
-                    filterValue={bezirk}
-                    typeOnClick={() => {
-                      removeFilter({
-                        state: bezirkeFilter,
-                        arrayStateSetter: setBezirkeFilter,
-                        filterType: "bezirke",
-                        value: bezirk,
-                        router,
-                        params,
-                        searchParams,
-                      });
-                    }}
-                  />
-                </React.Fragment>
-              ))}
-            {!!stadtteileFilter.length &&
-              stadtteileFilter.map((st) => (
-                <React.Fragment key={st}>
-                  <FilterButton
-                    filterType="Stadtteil"
-                    filterValue={st}
-                    typeOnClick={() => {
-                      removeFilter({
-                        state: stadtteileFilter,
-                        arrayStateSetter: setStadtteileFilter,
-                        filterType: "stadtteile",
-                        value: st,
-                        router,
-                        params,
-                        searchParams,
-                      });
-                    }}
-                  />
-                </React.Fragment>
-              ))}
-            {!!categoriesFilter.length &&
-              categoriesFilter.map((bz) => (
-                <React.Fragment key={bz}>
-                  <FilterButton
-                    filterType="Kategorie"
-                    filterValue={bz}
-                    typeOnClick={() => {
-                      removeFilter({
-                        state: categoriesFilter,
-                        arrayStateSetter: setCategoriesFilter,
-                        filterType: "categories",
-                        value: bz,
-                        router,
-                        params,
-                        searchParams,
-                      });
-                    }}
-                  />
-                </React.Fragment>
-              ))}
-          </div>
-        </div>
-      )}
-      <article className="flex flex-col w-full flex-grow p-2 pt-0">
-        <CardsListDisplayMemo cardPosts={displayList}>
-          <button
-            onClick={resetFilters}
-            className="p-2 bg-negative-500 text-hh-50 rounded bg-opacity-50 hover:bg-opacity-75 m-1"
-          >
-            Alle Filter zurücksetzen
-          </button>
-        </CardsListDisplayMemo>
+          </CardsListDisplayMemo>
 
-        {displayList.length < maxDisplayable && (
-          <button
-            onClick={() => setMaxDisplay((prev) => prev + 12)}
-            className="p-2 bg-hh-500 rounded bg-opacity-25 hover:bg-opacity-50"
-          >
-            Load More
-          </button>
-        )}
-      </article>
-    </section>
+          {displayList.length < maxDisplayable && (
+            <button
+              onClick={() => setMaxDisplay((prev) => prev + 12)}
+              className="p-2 bg-hh-500 rounded bg-opacity-25 hover:bg-opacity-50"
+            >
+              Load More
+            </button>
+          )}
+        </article>
+      </section>
+      <div className="w-96 aspect-[3/4]">
+        <DynamicGeneralMap>
+          <MarkersLists lists={{ posts: displayList }} />
+        </DynamicGeneralMap>
+      </div>
+    </div>
   );
 }
