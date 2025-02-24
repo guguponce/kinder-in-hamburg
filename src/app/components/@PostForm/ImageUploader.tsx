678921f21fd6b426ca0b171c859909ea3d45181f
guguponce <augustoponceph@gmail.com>
2025-02-25 00:11:17 +0100

    feat: refactor ImageUploader to integrate ImageUploaderUI and enhance image handling with WebP conversion

--- src/app/components/@PostForm/ImageUploader.tsx
+++ src/app/components/@PostForm/ImageUploader.tsx
@@ -1,18 +1,17 @@
 "use client";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "firebase/storage";
-import DownloadImageButton from "./DownloadImageButton";
-import DeleteImageButton from "./DeleteImageButton";
-import UploadImagePreview from "./UploadImagePreview";
 import { type FullMetadata } from "firebase/storage";
 import { getImagesURLs, uploadPostImage } from "@app/api/storageActions";
-import { filterExtraImages } from "@app/utils/functions";
+import { convertAllFilesToWebp } from "@app/utils/functions";
+import { ImageUploaderUI } from "./ImageUploaderUI";
 
 interface ImageUploaderProps {
   setImagesUrlsReady: React.Dispatch<
     React.SetStateAction<{
       ready: boolean;
       urls: string[];
+      metadata?: FullMetadata;
     }>
   >;
   id: number;
@@ -25,14 +24,15 @@ export const ImageUploader = ({
   email,
 }: ImageUploaderProps) => {
   const [previousImagesURLS, setPreviousImagesURLS] = useState<
-    Array<{ url: string; fileName: string; metadata: FullMetadata }>
+    Array<{ url: string; fileName: string; metadata?: FullMetadata }>
   >([]);
   const [imageFiles, setImageFiles] = useState<Array<File>>([]);
   const [imagesURLS, setImagesURLS] = useState<
-    Array<{ url: string; fileName: string; metadata: FullMetadata }>
+    Array<{ url: string; fileName: string; metadata?: FullMetadata }>
   >([]);
+  const [localImageUrl, setLocalImageUrl] = useState<string[]>([]);
   const [uploadStatus, setUploadStatus] = useState<
-    "await" | "uploading" | "success" | "error" | "paused"
+    "await" | "uploading" | "success" | "error" | "paused" | "converting"
   >("await");
   const fileInputRef = React.useRef<HTMLInputElement>(null);
 
@@ -53,14 +53,20 @@ export const ImageUploader = ({
     }
   }, [imagesURLS, setImagesUrlsReady, imageFiles, previousImagesURLS]);
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const selectedImages = filterExtraImages(
-      Array.from(e.target.files as FileList)
+  const handleImageChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUploadStatus("converting");
+    if (!e.target.files) return;
+    const { urls, files } = await convertAllFilesToWebp(
+      e.target.files,
+      1200,
+      300
     );
-    setImageFiles(selectedImages);
+    setImageFiles((prev) => [...prev, ...files]);
+    setLocalImageUrl((prev) => [...prev, ...urls]);
+    setUploadStatus("await");
   };
-
   const handleUpload = async () => {
+    if (!imageFiles.length) return;
     Promise.all(
       imageFiles.map((image) => {
         uploadPostImage(
@@ -74,146 +80,32 @@ export const ImageUploader = ({
     )
       .then(() => {
         setImageFiles([]);
-        fileInputRef.current!.value = "";
+        setLocalImageUrl([]);
+        console.log(fileInputRef.current!.value);
       })
       .catch((error) => {
         throw new Error("Error in HandleUpload", error);
       });
   };
-  const filesSize = useMemo(
-    () => imageFiles.reduce((acc, { size }) => acc + size, 0),
-    [imageFiles]
-  );
 
   return (
-    <div
-      id="images-upload-container"
-      className="bg-hh-800 p-4 text-gray-900 max-w-[800px] mx-auto text-sm w-full"
-    >
-      <h2 className="text-xl font-bold text-white underline">
-        Images<span className="ml-2 text-negative-200 no-underline">*</span>
-        <span className="text-sm text-negative-200 no-underline">
-          (Max. size for all images together: 10mb)
-        </span>
-      </h2>
-
-      <label htmlFor="images" className="text-sm leading-7 text-gray-100">
-        <input
-          type="file"
-          ref={fileInputRef}
-          accept="image/*"
-          multiple
-          name="images"
-          onChange={handleImageChange}
-          className="mt-4 flex max-w-[300px] flex-wrap rounded  border border-gray-300 bg-gray-100 bg-opacity-60 px-3 py-1 text-base leading-8 text-gray-900 outline-none transition-colors duration-200 ease-in-out focus:border-hh-600 focus:bg-white focus:ring-2 focus:ring-hh-700"
-        />
-      </label>
-
-      {!!imageFiles.length && (
-        <div className="my-4 rounded border-4 border-[hsl(0,0%,100%,0.6)] bg-black bg-opacity-20 p-4 text-white">
-          <div className="flex justify-between">
-            <h3 className="font-semibold italic">
-              Selected Image Files{" "}
-              <span className={filesSize > 10000000 ? "text-negative-200" : ""}>
-                (full size: {(filesSize / 1000000).toFixed(2)}
-                mb)
-              </span>
-            </h3>
-            (status: {uploadStatus === "await" ? "not uploaded" : uploadStatus})
-          </div>
-          <ol className="list-decimal">
-            {imageFiles.map((image) => (
-              <li className="ml-6" key={image.name}>
-                {image.name} - ({`${(image.size / 1000000).toFixed(2)} mb`})
-              </li>
-            ))}
-          </ol>
-          <div className="my-4 flex gap-4">
-            <button
-              className="rounded bg-positive-700 px-4 py-2 font-bold text-white hover:bg-yellow-700"
-              disabled={
-                !imageFiles.length ||
-                (uploadStatus !== "await" && uploadStatus !== "success")
-              }
-              onClick={() => handleUpload()}
-            >
-              Upload Images
-            </button>
-            <button
-              className="flex h-10 w-fit items-center justify-center rounded bg-negative-500 px-2 py-2 font-bold text-white hover:bg-negative-700 "
-              onClick={() => {
-                setImageFiles([]);
-                fileInputRef.current!.value = "";
-              }}
-            >
-              Cancel
-            </button>
-          </div>
-        </div>
-      )}
-
-      {(!!previousImagesURLS.length ||
-        !!imagesURLS.length ||
-        uploadStatus === "uploading") && (
-        <div className="my-4 min-h-[260px] rounded bg-[hsl(220,14%,96%)] bg-opacity-60 p-2">
-          {!!previousImagesURLS.length && (
-            <div id="previous-images-container" className="p-2 font-semibold">
-              <h3 className="italic text-gray-900">List of previous images</h3>
-              <div className="flex flex-wrap items-stretch justify-stretch gap-4 py-4">
-                {previousImagesURLS.map((imageUrl, imageIndex) => (
-                  <div
-                    className={`
-                         flex min-w-36 flex-col items-center  justify-between rounded border-2 border-green-900 p-2`}
-                    key={imageUrl.url}
-                  >
-                    <UploadImagePreview
-                      imageUrl={imageUrl.url}
-                      imageFileName={imageUrl.fileName}
-                    >
-                      <DeleteImageButton
-                        imageIndex={imageIndex}
-                        imageName={imageUrl.fileName}
-                        path={`postsImages/${id}`}
-                        setImagesArray={setPreviousImagesURLS}
-                      />
-                      <DownloadImageButton
-                        imageName={imageUrl.fileName}
-                        imageUrl={imageUrl.url}
-                      />
-                    </UploadImagePreview>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-          {!!imagesURLS.length && (
-            <div id="recent-images-container" className="p-2 font-semibold">
-              <h3 className="italic text-gray-900">Recently uploaded images</h3>
-              <div className="wrap flex items-stretch justify-stretch gap-4 py-4">
-                {imagesURLS.map((imageUrl, imageIndex) => (
-                  <div
-                    className={`flex w-36 flex-col items-center justify-between rounded  border-2 border-green-900 p-2`}
-                    key={imageUrl.url}
-                  >
-                    <UploadImagePreview
-                      imageUrl={imageUrl.url}
-                      imageFileName={imageUrl.fileName}
-                    >
-                      <DeleteImageButton
-                        imageIndex={imageIndex}
-                        imageName={imageUrl.fileName}
-                        path={`postsImages/${id}`}
-                        setImagesArray={setImagesURLS}
-                      />
-                    </UploadImagePreview>
-                  </div>
-                ))}
-              </div>
-            </div>
-          )}
-        </div>
-      )}
-    </div>
+    <ImageUploaderUI
+      fileInputRef={fileInputRef}
+      handleImageChange={handleImageChange}
+      handleUpload={handleUpload}
+      uploadStatus={uploadStatus}
+      imageFiles={imageFiles}
+      setImageFiles={setImageFiles}
+      setLocalImageUrl={setLocalImageUrl}
+      previousImagesURLS={previousImagesURLS}
+      imagesURLS={imagesURLS}
+      localImageUrl={localImageUrl}
+      setImagesURLS={setImagesURLS}
+      id={id}
+      setPreviousImagesURLS={setPreviousImagesURLS}
+      maxMB={3}
+      maxMBText="(Max. size for all images together: 3mb)"
+    />
   );
 };
 
