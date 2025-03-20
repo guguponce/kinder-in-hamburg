e7cea612bab73d7889fb6353f60a918dca376d37
guguponce <augustoponceph@gmail.com>
2025-03-20 14:32:04 +0100

    refactor: optimize BezirkIcon component with lazy loading, replace for Icons.tsx and remove unused Icons

--- src/app/components/@Icons/@BezirkIcon/BezirkIcon.tsx
+++ src/app/components/@Icons/@BezirkIcon/BezirkIcon.tsx
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { lazy } from "react";
+
+const AltonaIcon = lazy(() => import("./AltonaIcon"));
+const HarburgIcon = lazy(() => import("./harburgIcon"));
+const WandsbekIcon = lazy(() => import("./WandsbekIcon"));
+const EimsbuettelIcon = lazy(() => import("./EimsbuettelIcon"));
+const NordIcon = lazy(() => import("./NordIcon"));
+const MitteIcon = lazy(() => import("./MitteIcon"));
+const BergedorfIcon = lazy(() => import("./BergedorfIcon"));
+const HamburgIcon = lazy(() => import("./HamburgIcon"));
+const HamburgFilledIcon = lazy(() => import("./HamburgFilledIcon"));
 
 export default function BezirkIcon({
   color = "#33404D",
-  size = "24px",
+  size = "100%",
   bezirk,
 }: {
   color?: string;
   size?: string;
   bezirk: string;
 }) {
-  const bezirkIcons: { [x: string]: string } = {
-    altona: "altonaIcon.svg",
-    harburg: "harburgIcon.svg",
-    wandsbek: "wandsbekIcon.svg",
-    eimsbüttel: "eimsbuettelIcon.svg",
-    eimsbuettel: "eimsbuettelIcon.svg",
-    "hamburg-nord": "nordIcon.svg",
-    "hamburg-mitte": "mitteIcon.svg",
-    bergedorf: "bergedorfIcon.svg",
-    "ausserhalb hamburg": "bezirkeIcon.svg",
-    "außerhalb hamburg": "bezirkeIcon.svg",
-    "umland hamburg": "bezirkeIcon.svg",
-    hamburg: "bezirkeIcon.svg",
-  };
-  return (
-    <img
-      className={`bezirkIcon w-fit h-full object-contain saturate-0 hover:invert-[0.05] transition-all`}
-      src={"/assets/bezirke/" + bezirkIcons[bezirk.toLocaleLowerCase()]}
-      alt=""
-    />
-  );
+  const bezirkLower = bezirk.toLowerCase();
+  if (bezirkLower === "altona") return <AltonaIcon color={color} size={size} />;
+  if (bezirkLower === "harburg")
+    return <HarburgIcon color={color} size={size} />;
+  if (bezirkLower === "wandsbek")
+    return <WandsbekIcon color={color} size={size} />;
+  if (bezirkLower === "eimsbüttel")
+    return <EimsbuettelIcon color={color} size={size} />;
+  if (bezirkLower === "eimsbuettel")
+    return <EimsbuettelIcon color={color} size={size} />;
+  if (bezirkLower === "hamburg-nord")
+    return <NordIcon color={color} size={size} />;
+  if (bezirkLower === "hamburg-mitte")
+    return <MitteIcon color={color} size={size} />;
+  if (bezirkLower === "bergedorf")
+    return <BergedorfIcon color={color} size={size} />;
+  if (bezirkLower === "ausserhalb hamburg")
+    return <HamburgIcon color={color} size={size} />;
+  if (bezirkLower === "außerhalb hamburg")
+    return <HamburgIcon color={color} size={size} />;
+  if (bezirkLower === "umland hamburg")
+    return <HamburgFilledIcon color={color} size={size} />;
+  if (bezirkLower === "hamburg")
+    return <HamburgFilledIcon color={color} size={size} />;
 }
