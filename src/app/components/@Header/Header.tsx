ca411c28506b22719f8478f9687be0cb298a8498
guguponce <augustoponceph@gmail.com>
2025-04-10 22:32:57 +0200

    fix: adjust logo image height and improve styling for responsiveness

--- src/app/components/@Header/Header.tsx
+++ src/app/components/@Header/Header.tsx
@@ -13,9 +13,11 @@ export default async function Header() {
       id="header"
       className="relative h-20 w-full lg:max-w-[1200px] bg-negative-400 flex justify-between lg:justify-center items-center gap-2 text-hh-50"
     >
+      {/* <div className="burgerContainer w-14 h-full flex flex-col justify-center"> */}
       <AdminServerComponent>
         <BurgerMenu />
       </AdminServerComponent>
+      {/* </div> */}
       <Link
         href={"/"}
         className="h-full overflow-hidden px-4 flex-grow rounded-sm flex items-center justify-center lg:justify-start gap-4"
@@ -25,17 +27,17 @@ export default async function Header() {
           src="/assets/logo/WhiteLogo-NoText.png"
           alt="Kinder in Hamburg"
           width={120}
-          height={85}
+          height={80}
           priority
-          className="hidden lg:flex object-contain text-sm brightness-105"
+          className="w-auto h-auto max-h-full hidden lg:flex object-contain text-sm brightness-105"
         />
         <Image
           src="/assets/logo/WhiteLogo-Transparent.png"
           alt="Kinder in Hamburg"
           width={120}
-          height={85}
+          height={80}
           priority
-          className="lg:hidden object-contain text-sm brightness-105"
+          className="w-auto h-auto max-h-full lg:hidden object-contain text-sm brightness-105"
         />
 
         <h1
