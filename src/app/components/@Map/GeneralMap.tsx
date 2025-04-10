97c9bddb9540e0945a513ea49673ce7bb079975d
guguponce <augustoponceph@gmail.com>
2025-04-10 22:20:52 +0200

    feat: add user location marker and enhance map centering logic in GeneralMap component

--- src/app/components/@Map/GeneralMap.tsx
+++ src/app/components/@Map/GeneralMap.tsx
@@ -14,6 +14,7 @@ import SpielplatzPopUP from "@app/components/@Map/PopUpsMarkers/SpielplatzPopUP"
 import FlohmarktPopUP from "./PopUpsMarkers/FlohmarktPopUP";
 import PostPopUP from "./PopUpsMarkers/PostPopUP";
 import { createStandortMapIcon } from "./functions";
+import { UserLocationIcon } from "./MarkerIcons";
 
 const MainLocationIcon = divIcon({
   html: createStandortMapIcon("#b72f1e", 35),
@@ -26,22 +27,41 @@ const GeneralMap = ({
   currentTarget,
   children,
   zoom = 10,
+  userLocation,
+  centerUserLocation = false,
 }: {
   zoom?: number;
   children?: React.ReactNode;
   currentTarget?: iPost | iFlohmarkt | iSpielplatz;
+  userLocation?: { lat: number; lon: number } | null;
+  centerUserLocation?: boolean;
 }) => {
   return (
     <MapContainer
       style={{ height: "100%", width: "100%", zIndex: 10 }}
-      center={[currentTarget?.lat || 53.5511, currentTarget?.lon || 9.9937]}
+      center={[
+        centerUserLocation && userLocation
+          ? userLocation?.lat
+          : currentTarget?.lat || 53.5511,
+        centerUserLocation && userLocation
+          ? userLocation.lon
+          : currentTarget?.lon || 9.9937,
+      ]}
       zoom={zoom}
       scrollWheelZoom={true}
+      closePopupOnClick
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      {userLocation && (
+        <Marker
+          position={[userLocation.lat, userLocation.lon]}
+          icon={UserLocationIcon}
+        />
+      )}
+
       {currentTarget?.lat && currentTarget?.lon && (
         <Marker
           position={[currentTarget.lat, currentTarget.lon]}
