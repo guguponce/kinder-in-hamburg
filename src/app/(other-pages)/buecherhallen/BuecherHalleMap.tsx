b443ea3641245e90e746cc5b80b64d50108cfc2e
guguponce <augustoponceph@gmail.com>
2025-04-14 12:37:42 +0200

    refactor: streamline imports and update custom icon handling in BuecherHalleMap component

--- src/app/(other-pages)/buecherhallen/BuecherHalleMap.tsx
+++ src/app/(other-pages)/buecherhallen/BuecherHalleMap.tsx
@@ -1,9 +1,7 @@
 import { iPost } from "@app/utils/types";
 import dynamic from "next/dynamic";
 import React from "react";
-import { buecherhalleDivIcon } from "@app/components/@Map/mapUtils/constants";
-import MarkersLists from "@app/components/@Map/PopUpsMarkers/MarkersLists";
-import BuecherhalleIcon from "@app/components/@Icons/BuecherhalleIcon";
+import BuecherhalleIcon from "@components/@Icons/BuecherhalleIcon";
 
 const GeneralMap = dynamic(() => import("@components/@Map/GeneralMap"), {
   ssr: false,
@@ -17,6 +15,12 @@ const GeneralMap = dynamic(() => import("@components/@Map/GeneralMap"), {
     </article>
   ),
 });
+const MarkersLists = dynamic(
+  () => import("@components/@Map/PopUpsMarkers/MarkersLists"),
+  {
+    ssr: false,
+  }
+);
 export default function BuecherHalleMap({
   buecherhallenPosts,
 }: {
@@ -26,8 +30,9 @@ export default function BuecherHalleMap({
     <div className="w-full max-w-[800px] aspect-square sm:aspect-video max-h-[60vh] bg-hh-800 rounded-lg p-1 sm:py-2 flex flex-col gap-2 overflow-hidden">
       <GeneralMap centerUserLocation>
         <MarkersLists
+          cluster={false}
           lists={{ posts: buecherhallenPosts }}
-          customIcon={buecherhalleDivIcon}
+          customIcon={"buecherhalle"}
           popUpIcon={<BuecherhalleIcon />}
         />
       </GeneralMap>
