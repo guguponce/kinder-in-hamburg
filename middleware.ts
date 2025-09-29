e0dca144b4c9322b14ff37893c4735a11ad179c7
guguponce <augustoponceph@gmail.com>
2025-09-29 21:13:33 +0200

    fix: streamline middleware to redirect uppercase URLs to lowercase

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,15 @@
-import { type NextRequest } from "next/server";
+import { type NextRequest, NextResponse } from "next/server";
 import { updateSession } from "@auth/middleware";
 
 export async function middleware(request: NextRequest) {
+  // Redirect uppercase URLs to lowercase
+  if (request.nextUrl.pathname !== request.nextUrl.pathname.toLowerCase()) {
+    const url = request.nextUrl.clone();
+    url.pathname = url.pathname.toLowerCase();
+    return NextResponse.redirect(url);
+  }
+
+  // Run auth session middleware
   return await updateSession(request);
 }
 
