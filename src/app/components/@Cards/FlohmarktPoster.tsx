d685260b34932ced9dd9e150437cc78c41b2edf4
guguponce <augustoponceph@gmail.com>
2025-10-08 14:18:10 +0200

    fix: refactor FlohmarktPoster component to improve className handling and enhance layout for better responsiveness

--- src/app/components/@Cards/FlohmarktPoster.tsx
+++ src/app/components/@Cards/FlohmarktPoster.tsx
@@ -1,4 +1,4 @@
-import { getDate } from "@app/utils/functions";
+import { cn, getDate } from "@app/utils/functions";
 import Link from "next/link";
 import React from "react";
 import FlohmarktBackground from "../@Icons/@Flohmarkt/FlohmarktBackground";
@@ -48,26 +48,36 @@ export default function FlohmarktPoster({
             }
           : {}
       }
-      className={`relative ${
-        image ? "" : "bg-gradient-to-b from-[#e1e4e59f] to-[#d0d7da9f] p-2"
-      } w-full ${
+      className={cn(
+        `relative w-full h-full shadow-md rounded flex flex-col items-center justify-between text-center hover:scale-[1.01] hover:shadow-xl focus-visible:border-2 focus-visible:border-hh-900`,
+        image ? "" : "bg-gradient-to-b from-[#e1e4e59f] to-[#d0d7da9f] p-2",
         size === "small" ? "min-w-[144px]" : "min-w-[180px]"
-      } h-full shadow-md rounded flex flex-col items-center justify-between text-center hover:scale-[1.01] hover:shadow-xl focus-visible:border-2 focus-visible:border-hh-900`}
+      )}
     >
-      {!image &&
-        (eventType === "flohmarkt" ? (
-          <FlohmarktBackgroundMemo
-            randomNumber={
-              typeof index === "number"
-                ? Number("0." + (index + 1) * 72584270527)
-                : undefined
-            }
-          />
-        ) : (
-          eventType === "laterne" ||
-          (eventType === "laternewerkstatt" && (
+      {image ? (
+        <div
+          className="absolute w-full h-full top-0 left-0 blur-sm"
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        />
+      ) : eventType === "flohmarkt" ? (
+        <FlohmarktBackgroundMemo
+          randomNumber={
+            typeof index === "number"
+              ? Number("0." + (index + 1) * 72584270527)
+              : undefined
+          }
+        />
+      ) : (
+        (eventType === "laterne" || eventType === "laternewerkstatt") && (
+          <>
+            <div className="absolute w-[30%] aspect-square rounded-full -translate-x-full top-1/2 left-[48%] bg-gradient-radial from-orange-300 via-transparent to- transparent  z-20" />
             <div
-              className="absolute w-full h-full flex justify-between flex-col items-center text-orange-200 border-2 border-hh-600 rounded overflow-hidden"
+              className="absolute w-full h-full top-0 bg-hh-950 flex justify-between flex-col items-center text-orange-200 border-2 border-hh-600 rounded overflow-hidden"
               style={{
                 backgroundImage: `url("/assets/icons/laterne/laterne.svg")`,
                 backgroundSize: "50%",
@@ -75,8 +85,9 @@ export default function FlohmarktPoster({
                 backgroundRepeat: "no-repeat",
               }}
             />
-          ))
-        ))}
+          </>
+        )
+      )}
       {!!image && !["adventsevent", "weihnachtsmarkt"].includes(eventType) ? (
         <img
           loading="lazy"
@@ -103,30 +114,47 @@ export default function FlohmarktPoster({
             />
           ) : null}
           <h2
-            className={`text-sm font-extrabold p-2 text-positive-900 my-4 max-w-[180px] break-words rounded bg-hh-300 backdrop-blur-sm ${image ? "bg-opacity-80" : "bg-opacity-50"}`}
+            className={cn(
+              "text-sm font-extrabold p-2 my-4 max-w-[180px] break-words rounded bg-hh-300 backdrop-blur-sm text-positive-900",
+              image
+                ? "bg-opacity-80"
+                : eventType === "laterne" || eventType === "laternewerkstatt"
+                  ? "text-orange-300 bg-opacity-30"
+                  : "bg-opacity-50"
+            )}
           >
             {title}
           </h2>
           <div
-            className={`flex flex-col items-center p-1 mb-4 bg-hh-300 bg-opacity-50 backdrop-blur-sm rounded mx-1 ${image ? "bg-opacity-75" : "bg-opacity-50"}`}
+            className={cn(
+              "flex flex-col items-center p-1 mb-4 bg-hh-300 bg-opacity-50 backdrop-blur-sm rounded mx-1 font-semibold",
+              image
+                ? "bg-opacity-80"
+                : eventType === "laterne" || eventType === "laternewerkstatt"
+                  ? "text-orange-300 bg-opacity-30"
+                  : "bg-opacity-50"
+            )}
           >
-            <h2 className="text-base font-semibold text-hh-800">
-              {stadtteil || bezirk}
-            </h2>
+            <h2 className="text-base ">{stadtteil || bezirk}</h2>
             {endDate ? (
-              <h3 className="text-xs font-semibold text-hh-800">
+              <h3
+                className={cn(
+                  "text-xs ",
+                  ["laterne", "laternewerkstatt"].includes(eventType)
+                    ? "text-orange-200"
+                    : "text-hh-800"
+                )}
+              >
                 {date > today
                   ? `Ab dem ${getDate(date)}`
                   : `Bis zum ${getDate(endDate)}`}
               </h3>
             ) : getDate(date) === getDate(today) ? (
-              <h3 className="text-sm font-semibold text-hh-800 outline-2 outline-hh-800 rounded w-full outline">
+              <h3 className="text-sm  outline-2 outline-hh-800 rounded w-full outline">
                 Heute
               </h3>
             ) : (
-              <h3 className="text-sm font-semibold text-hh-800">
-                {getDate(date)}
-              </h3>
+              <h3 className="text-sm ">{getDate(date)}</h3>
             )}
           </div>
         </>
