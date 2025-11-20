13db20a163c5212b5c72e4d00a78ba30aa5ce808
guguponce <augustoponceph@gmail.com>
2025-11-20 12:01:03 +0100

    feat: update OldEventSign component to include closed date logic and improve text display

--- src/app/(events)/events/[eventID]/OldEventSign.tsx
+++ src/app/(events)/events/[eventID]/OldEventSign.tsx
@@ -1,4 +1,4 @@
-import { getTodayNexMonday } from "@app/utils/functions";
+import { getDate, getTodayNexMonday } from "@app/utils/functions";
 import Link from "next/link";
 import React from "react";
 
@@ -7,14 +7,16 @@ export default function OldEventSign({
   text,
   date,
   endDate,
+  closedDates,
 }: {
   title?: string;
   text?: string;
   date: number;
   endDate?: number;
+  closedDates?: number[];
 }) {
   const { yesterdayEvening, today } = getTodayNexMonday();
-
+  const isClosedToday = closedDates?.some((d) => getDate(d) === getDate(today));
   const isNotCurrent = endDate
     ? endDate < yesterdayEvening
     : date < yesterdayEvening;
@@ -22,10 +24,12 @@ export default function OldEventSign({
   return (
     <aside className="flex flex-col items-center justify-center p-6 rounded-md bg-negative-100 border-4 border-negative-300 w-[500px] max-w-full m-2 gap-4">
       <h2 className="text-lg font-bold text-hh-950">
-        {title || "Dieser Event ist bereits vorbei."}
+        {isClosedToday
+          ? "Heute findet diese Veranstaltung nicht statt"
+          : title || "Dieser Event ist bereits vorbei."}
       </h2>
       <p className="text-base text-hh-600">
-        {text || "Schaue dich mal die zukunftigen Events an"}
+        {text || "Schaue dich mal die anderen Events an"}
       </p>
       <Link
         className="p-2 rounded-md bg-hh-800 hover:bg-hh-700 active:bg-hh-600 text-white"
