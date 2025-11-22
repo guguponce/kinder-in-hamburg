c20d09741086fdb22f0738383a710c933d013418
guguponce <augustoponceph@gmail.com>
2025-11-22 18:34:06 +0100

    feat: update Home component to include WeihnachtsBanner and adjust BaldBanner visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import FlohmaerkteBanner from "./components/@Index/FlohmaerkteContainer";
 import SommerInHamburgBanner from "./components/@Index/sommerinhamburg/SommerInHamburgBanner";
 import LaternenBanner from "./components/@Index/laternenumzug/LaternenBanner";
 import { BaldBanner } from "./components/BaldBanner";
+import WeihnachtsBanner from "./components/@Index/WeihnachtsBanner";
 
 export default function Home() {
   return (
@@ -12,11 +13,11 @@ export default function Home() {
       className="w-full flex flex-col items-center gap-4 xs:px-2"
     >
       <div className="w-full flex gap-4 justify-center items-stretch flex-wrap">
+        <WeihnachtsBanner />
         <LaternenBanner />
-        <BaldBanner className="hidden md:flex w-full sm:max-w-[440px] items-stretch" />
       </div>
       <FlohmaerkteBanner />
-      <BaldBanner className="md:hidden flex w-full sm:max-w-[640px] items-stretch" />
+      <BaldBanner className="flex w-full sm:max-w-[640px] items-stretch" />
 
       <SommerInHamburgBanner></SommerInHamburgBanner>
       <ShortAbout></ShortAbout>
