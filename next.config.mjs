88c632cdfc040f0de071d13286adbde13dcd158c
guguponce <augustoponceph@gmail.com>
2026-04-20 13:01:37 +0200

    refactor: restructure images configuration for improved clarity and organization

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nnozdpexhclustqdgdwh.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+    domains: [
+      "nnozdpexhclustqdgdwh.supabase.co",
+      "buergerbeteiligung.rellingen.de",
+    ],
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
