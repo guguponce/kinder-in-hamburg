b247c8f0b9f3fb29a76d7903559af552eb40ac8e
guguponce <augustoponceph@gmail.com>
2026-05-21 11:56:40 +0200

    feat: restructure Header component layout and integrate AdminClientComponent for user location button

--- src/app/components/@Header/Header.tsx
+++ src/app/components/@Header/Header.tsx
@@ -6,6 +6,7 @@ import UserButtons from "./UserButtons";
 import AdminServerComponent from "@app/providers/AdminServerComponents";
 import NavLinks from "./NavLinks";
 import UserLocationButton from "../@Map/UserLocationModal";
+import AdminClientComponent from "@app/providers/AdminClientComponents";
 
 export default async function Header() {
   return (
@@ -15,12 +16,14 @@ export default async function Header() {
     >
       {/* <div className="burgerContainer w-14 h-full flex flex-col justify-center"> */}
       <AdminServerComponent>
-        <BurgerMenu />
+        <div className="flex-1 flex items-center justify-start lg:hidden">
+          <BurgerMenu />
+        </div>
       </AdminServerComponent>
       {/* </div> */}
       <Link
         href={"/"}
-        className="h-full overflow-hidden px-4 flex-grow rounded-sm flex items-center justify-center lg:justify-start gap-4"
+        className="h-full overflow-hidden px-4 rounded-sm flex items-center justify-center lg:justify-start gap-4"
         id="main-logo-link"
       >
         <img
@@ -43,14 +46,17 @@ export default async function Header() {
           <span>in Hamburg</span>
         </h1>
       </Link>
-      <div className="hidden lg:flex">
+      <div className="hidden lg:flex flex-grow justify-end">
         <NavLinks />
       </div>
-      {/* <SharingButton /> */}
-      <AdminServerComponent>
-        <UserLocationButton />
-        <UserButtons />
-      </AdminServerComponent>
+      <div className="flex-1 flex items-center justify-end gap-2">
+        <AdminClientComponent>
+          <UserLocationButton minimal />
+        </AdminClientComponent>
+        <AdminServerComponent>
+          <UserButtons />
+        </AdminServerComponent>
+      </div>
     </header>
   );
 }
